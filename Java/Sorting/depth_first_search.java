84bc3824da3b6115acbf55868b477377621e3b66
Vishal Kumar Singh <01vksingh@gmail.com>
2022-10-28 20:03:58 +0530

    updated depth first search

--- Java/Sorting/depth_first_search.java
+++ Java/Sorting/depth_first_search.java
@@ -1,46 +1,51 @@
-import java.util.*;
-
-class Graph {
-  private LinkedList<Integer> adjLists[];
-  private boolean visited[];
-
-  // Graph
-  Graph(int vertices) {
-    adjLists = new LinkedList[vertices];
-    visited = new boolean[vertices];
-
-    for (int i = 0; i < vertices; i++)
-      adjLists[i] = new LinkedList<Integer>();
-  }
-
-  // edges
-  void addEdge(int src, int dest) {
-    adjLists[src].add(dest);
-  }
-
-  // algorithm
-  void DFS(int vertex) {
-    visited[vertex] = true;
-    System.out.print(vertex + " ");
-
-    Iterator<Integer> ite = adjLists[vertex].listIterator();
-    while (ite.hasNext()) {
-      int adj = ite.next();
-      if (!visited[adj])
-        DFS(adj);
-    }
-  }
-
-  public static void main(String args[]) {
-    Graph g = new Graph(4);
-
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 3);
-
-    System.out.println("Following is Depth First Traversal");
-
-    g.DFS(2);
-  }
-}
+import java.util.*;  
+  
+class DFSTraversal {  
+  private LinkedList<Integer> adj[]; /*adjacency list representation*/  
+  private boolean visited[];  
+  
+  /* Creation of the graph */  
+  DFSTraversal(int V) /*'V' is the number of vertices in the graph*/  
+  {  
+    adj = new LinkedList[V];  
+    visited = new boolean[V];  
+  
+    for (int i = 0; i < V; i++)  
+      adj[i] = new LinkedList<Integer>();  
+  }  
+  
+  /* Adding an edge to the graph */  
+  void insertEdge(int src, int dest) {  
+    adj[src].add(dest);  
+  }  
+  
+  void DFS(int vertex) {  
+    visited[vertex] = true; /*Mark the current node as visited*/  
+    System.out.print(vertex + " ");  
+  
+    Iterator<Integer> it = adj[vertex].listIterator();  
+    while (it.hasNext()) {  
+      int n = it.next();  
+      if (!visited[n])  
+        DFS(n);  
+    }  
+  }  
+  
+  public static void main(String args[]) {  
+    DFSTraversal graph = new DFSTraversal(8);  
+  
+        graph.insertEdge(0, 1);    
+        graph.insertEdge(0, 2);    
+        graph.insertEdge(0, 3);    
+        graph.insertEdge(1, 3);    
+        graph.insertEdge(2, 4);  
+        graph.insertEdge(3, 5);       
+        graph.insertEdge(3, 6);    
+        graph.insertEdge(4, 7);    
+        graph.insertEdge(4, 5);    
+        graph.insertEdge(5, 2);    
+          
+        System.out.println("Depth First Traversal for the graph is:");    
+        graph.DFS(0);    
+  }  
+} 
