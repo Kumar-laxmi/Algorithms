c3eb6d0f7fff2874974c86fb248e9e2f9b924e36
Mayur Jethwa <69671324+mayyyur@users.noreply.github.com>
2023-01-01 21:43:58 +0530

    Updated program file

--- C/Greedy-Algorithm/fractional_knapsack.c
+++ C/Greedy-Algorithm/fractional_knapsack.c
@@ -1,95 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
-void main ()
-{
-int n, m, w[100], p[100], ratio[100] , i, j, u, temp;
-float xr, x[100], total_profit=0, total_weight=0;
+
+void main() {
+int n,weight[10],profit[10],W,i;  
+
 printf ("Enter the number of items(n): ");
 scanf ("%d", &n);
 printf ("Enter the capacity of the Knapsack(m): ");
-scanf ("%d", &m);
-u = m;
-for(i=0;i<n;i++)
-{
-x[i]=0;
-}
+scanf ("%d", &W);
+
 printf ("Enter the Weights of items: \n");
 for (i = 0; i < n; i++)
 {
 printf ("Weight of item %d = ", i + 1);
-scanf ("%d", &w[i]);
+scanf ("%d", &weight[i]);
 }
 printf ("\nEnter the Profit Values of items: \n");
 for (i = 0; i < n; i++)
 {
 printf ("Profit of item %d = ", i + 1);
-scanf ("%d", &p[i]);
+scanf ("%d", &profit[i]);
 }
-for (i = 0; i < n; i++)
-{
-ratio[i] = p[i] / w[i];
-}
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n - 1; j++)
-{
-if (ratio[j] < ratio[i])
-{
-temp = ratio[i];
-ratio[i] = ratio[j];
-ratio[j] = temp;
-temp = w[i];
-w[i] = w[j];
-w[j] = temp;
-temp = p[i];
-p[i] = p[j];
-p[j] = temp;
-}
-}
-}
-printf("\nItem:\t\t");
-for(i=0;i<n;i++)
-{
-printf("%d\t",i+1);
-}
-printf("\nProfit:\t\t");
-for(i=0;i<n;i++)
-{
-printf("%d\t",p[i]);
-}
-printf("\nWeights:\t");
-for(i=0;i<n;i++)
-{
-printf("%d\t",w[i]);
-}
-printf ("\nRATIO:\t\t");
-for (i = 0; i < n; i++)
-{
-printf ("%d\t", ratio[i]);
-}
-for(i=0;i<n;i++)
-{
-if(w[i]<=u)
-{
-x[i]=1;
-u=u-w[i];
-}
-else if(w[i]>u)
-{
-break;
-}
-}
-if(i<=n)
-{
-xr = (float)u/w[i];
-x[i] = xr;
-}
-for(i=0;i<n;i++)
-{
-total_profit += x[i]*p[i];
-total_weight += x[i]*w[i];
-}
-printf("\nTotal Profit = %.2f \nTotal Weight = %.2f",total_profit,total_weight);
-getch();
+
+    int cur_w;
+    float tot_v;
+    int maxi;
+    int used[10];
+ 
+    for (i = 0; i < n; ++i){
+        used[i] = 0;
+    }
+ 
+    cur_w = W;
+    while (cur_w > 0) { 
+        
+        maxi = -1;
+        for (i = 0; i < n; ++i)
+            if ((used[i] == 0) &&
+                ((maxi == -1) || ((float)profit[i]/weight[i] > (float)profit[maxi]/weight[maxi])))
+                maxi = i;
+ 
+        used[maxi] = 1; 
+        cur_w -= weight[maxi]; 
+        tot_v += profit[maxi];
+        if (cur_w >= 0)
+            printf("Added item %d (%d$, %dKg) completely in the bag. Space left: %d.\n", maxi + 1, profit[maxi], weight[maxi], cur_w);
+        else {
+            printf("Added %d%% (%d$, %dKg) of item %d in the bag.\n", (int)((1 + (float)cur_w/weight[maxi]) * 100), profit[maxi], weight[maxi], maxi + 1);
+            tot_v -= profit[maxi];
+            tot_v += (1 + (float)cur_w/weight[maxi]) * profit[maxi];
+        }
+    }
+ 
+    printf("Filled the bag with items worth %.2f$.\n", tot_v);
+    getch();
 }
