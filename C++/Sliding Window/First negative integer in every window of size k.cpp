0a7375c11d4e899289f5ba178bf6c2f55ea09816
Shubham Yadav <shubh08am@gmail.com>
2023-01-06 10:02:14 +0530

    Update First negative integer in every window of size k.cpp

--- C++/Sliding Window/First negative integer in every window of size k.cpp
+++ C++/Sliding Window/First negative integer in every window of size k.cpp
@@ -2,13 +2,11 @@ Question:- Given an array and a positive integer k, find the first negative inte
  If a window does not contain a negative integer, then print 0 for that window.
   
 vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int k) {
-            
+                                             long long int N, long long int k) {  
             vector<long long int>ans,res;
             int i=0,j=0;
             while(j<N){
                  if(A[j]<0) ans.push_back(A[j]) ;
-                 
                  if(j-i+1 < k) {j++;}
                  else if(j-i+1==k){
                      if(ans.size()==0) res.push_back(0);
