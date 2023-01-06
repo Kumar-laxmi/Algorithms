40a50b69878265a0d625e9ee45e9f71f360f44b3
Shubham Yadav <shubh08am@gmail.com>
2023-01-06 10:05:01 +0530

    Update Substrings of length k with k-1 distinct elements.cpp

--- C++/Sliding Window/Substrings of length k with k-1 distinct elements.cpp
+++ C++/Sliding Window/Substrings of length k with k-1 distinct elements.cpp
@@ -6,18 +6,14 @@ class Solution {
     int countOfSubstrings(string S, int K) {
         // code here
         int i=0,j=0,cnt=0;
-        map<char,int>mp;
-        
+        map<char,int>mp;        
         while(j<S.size()){
             mp[S[j]]++;
             if(j-i+1<K) {
                 j++;
-            }
-            
-            else if(j-i+1==K){
-                
-                if(mp.size()==K-1) cnt++;
-                
+            }            
+            else if(j-i+1==K){             
+                if(mp.size()==K-1) cnt++;                
                 mp[S[i]]--;
                 if(mp[S[i]]==0) mp.erase(S[i]);
                 i++;
