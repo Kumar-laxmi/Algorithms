f70d7e216dd1c7545c3530e1a24e20f10dd37cb3
Shubham Yadav <shubh08am@gmail.com>
2023-01-06 10:06:42 +0530

    Update Sieve of Eratosthenes Algorithm.cpp

--- C++/Maths/Sieve of Eratosthenes Algorithm.cpp
+++ C++/Maths/Sieve of Eratosthenes Algorithm.cpp
@@ -3,7 +3,6 @@ Time complexity of Sieve of Eratosthenes is n*log(log(n))
 It is one of the most efficient ways to find all prime numbers.
 */
 
-
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
@@ -22,24 +21,17 @@ void sieveOfEratosthenes() {
              } 
          }
     }
-    
     for(int p=2; p<=N; p++) if(isprime[p]) {
         sieve.push_back(p);
     }
 }
-
-
 signed main(){
-
 int n  ;
 cin>>n;
-
 cout<<"First "<<n<<" prime numbers are\n";
 //print first n prime numbers
 sieveOfEratosthenes(); // calling sieve to display prime numbers
 for(int i=0;i<n;i++){
     cout<<sieve[i]<<" ";
 }
-
-
 return 0;}
