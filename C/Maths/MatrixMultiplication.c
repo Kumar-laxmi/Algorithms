65b304b34e5d05340c3ac5946037ebd7a17cd589
Dinesh Nalam <dbabu5612@gmail.com>
2023-01-07 15:29:41 +0530

    added matrix multiplication in c, c++, java & python

---
+++ C/Maths/MatrixMultiplication.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define N 3
+// function to multiply two matrices
+void multiply(int a[][N], int b[][N], int c[][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            c[i][j] = 0;
+            for (int k = 0; k < N; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+int main()
+{
+    int a[N][N] = {{1, 2, 3},
+                   {4, 5, 6},
+                   {7, 8, 9}};
+    int b[N][N] = {{9, 8, 7},
+                   {6, 5, 4},
+                   {3, 2, 1}};
+    int c[N][N];  // empty matrix to store the result
+
+    multiply(a, b, c);
+    // print the result
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
