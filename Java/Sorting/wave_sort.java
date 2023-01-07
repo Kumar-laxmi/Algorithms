0cf07272d9931f99cd4d86dcb8166a4b227fa297
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2023-01-07 14:22:01 +0530

    wave sort code in java

---
+++ Java/Sorting/wave_sort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class WaveSort {
+    public static void waveSort(int[] arr) {
+        // Sorting the array in ascending order
+        Arrays.sort(arr);
+
+        // Swapping the elements in pairs
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        waveSort(arr);
+
+        System.out.println(Arrays.toString(arr));  // [2, 1, 4, 3, 6, 5, 8, 7, 9]
+    }
+}
