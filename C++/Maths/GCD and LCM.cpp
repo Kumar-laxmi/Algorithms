f25e65dc86ee74afd012043f2f50c2905acc76ad
Shubham Yadav <shubh08am@gmail.com>
2023-01-07 10:28:06 +0530

    Update and rename GCD and LCM.md to GCD and LCM.cpp

--- C++/Maths/GCD and LCM.cpp
+++ C++/Maths/GCD and LCM.cpp
@@ -1,35 +1,27 @@
-# GCD and LCM
- 
-# GCD
+/* 
+•GCD
 Greatest common divisor (GCD) of two or more numbers is greatest common factor number that divides them.
 
-# Example
-```
+ •Example
 Greatest common divisor of 5 and 10 is 5, because both the numbers are divisible by 5.
 5/5 = 1
 10/5 = 2
-```
 
-# Method to Find GCD
-```
+•Method to Find GCD
 In order to find gcd of numbers, all factors of the numbers is needed to be determined and find the largest common factor. 
 Example, 2, 4 and 8 are three numbers. Then the factors of 2, 4 and 8 are:
 2 → 1,2
 4 → 1,2,4
 8 → 1,2,4,8
 Thus, it can be concluded that 2 is the highest common factor among all three numbers. Hence, GCD of 2,4 and 8 is 2
-```
 
-# LCM
+•LCM
 LCM (Least Common Multiple) of two numbers is the smallest number which can be divided by both numbers.
 
-# Example
-```
+• Example
 Least Common Multiple of 2 and 5 is 10. As, 10 is divisible by both 2 and 5. Such, that 2 and 5 are known as the divisors of 10.
-```
 
-# Method to Find LCM :- Using Prime Factors
-```
+• Method to Find LCM :- Using Prime Factors
 Example, 12 and 16 are two numbers. Writing factors of both the numbers individually.
 • STEP-1
 12 = 2 x 2 x 3
@@ -41,29 +33,24 @@ Prime factors of both number is :-
 Pair all common prime factors and that gives LCM.
 Thus, there are one 3 and four 2’s.
 LCM (12, 16) = 2 x 2 x 2 x 2 x 3 = 48
-```
 
-# Relation Between LCM and GCD
-```
+• Relation Between LCM and GCD
 If a and b are given numbers.Then:
 a x b = LCM(a, b) * GCD (a, b)
 LCM(a, b) = (a x b) / GCD(a, b)
 ```
  
-# CODE 
-
-### Sample Input
-```
+•Sample Input
 a=4 , b=8
-```
-### Sample Output
-```
+
+•Sample Output
 GCD = 4
 LCM = 8
-```
 
-### Solution
-```cpp
+•Time Complexity
+O(log(min(a,b)) , where a and b are the numbers whose GCD and LCM are determined.
+*/  
+
  #include<bits/stdc++.h>
 //#include <boost/math/common_factor.hpp>
 using namespace std;
@@ -96,7 +83,3 @@ cout << __gcd(a,b) << endl;  /* ----> T.c -> O(logn)*/
 /* MINIMUM FRACTION application of gcd*/
     return 0;
 }
-```
-
-# Time Complexity
-O(log(min(a,b)) , where a and b are the numbers whose GCD and LCM are determined.
