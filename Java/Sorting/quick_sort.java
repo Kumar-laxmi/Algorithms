32fd4a6aa3d46e2e9110baef4276bd84195d4fd7
unknown <goyalhim69@gmail.com>
2023-01-08 12:06:07 +0530

    added quick_sort.java sorting algorithm

--- Java/Sorting/quick_sort.java
+++ Java/Sorting/quick_sort.java
@@ -1,27 +1,18 @@
-import java.io.*;
 import java.util.Scanner;
 
 class quick_sort{
-
 	//function to swap values in array
-	void swap_values(int[] arr, int i, int j)
-	{
+	void swap_values(int[] arr, int i, int j){
 		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
-	
-	//partition function
 	//puts all the values<pivot to left of pivot
 	//and greater to the left
-	int partition(int[] arr, int low, int high)
-	{
+	int partition(int[] arr, int low, int high){
 		int pivot = arr[high];
-
 		int i = (low - 1);
-
 		for (int j = low; j <= high - 1; j++) {
-
 			if (arr[j] < pivot) {
 				i++;
 				swap_values(arr, i, j);
@@ -30,12 +21,9 @@ class quick_sort{
 		swap_values(arr, i + 1, high);
 		return (i + 1);
 	}
-
-    	//function for quick sort
-	void quickSort(int[] arr, int low, int high)
-	{
+    //function for quick sort
+	void quickSort(int[] arr, int low, int high){
 		if (low < high) {
-
 			int pivot = partition(arr, low, high);
 			
 			quickSort(arr, low, pivot - 1);
@@ -45,24 +33,20 @@ class quick_sort{
 
 	public static void main(String[] args)
 	{
-	    Scanner s = new Scanner(System.in);
-	    
+	    Scanner s = new Scanner(System.in);    
 	    //enter array size
         int n = s.nextInt();
-        
         //initialize array with size n
         int[] arr = new int[n];
-        
         //input the array
         for(int i=0;i<n;i++){
             arr[i] = s.nextInt();   
         }
-		
-		quick_sort sortArray = new quick_sort();
+		s.close();
 
+		quick_sort sortArray = new quick_sort();
         //call the quickSort function
 		sortArray.quickSort(arr, 0, n - 1);
-		
 		//print the array after sorting
 		System.out.println("Sorted array: ");
 		for (int i = 0; i < n; i++){
