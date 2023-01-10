cc9d6f5acfb639a9eb8fff283a94091b51a8d91d
Mansi Uniyal <85786030+Mansiuniyal@users.noreply.github.com>
2023-01-10 16:28:59 +0530

    Shell sort.c
    (Updated code)

--- C/Sorting/Shell_sort.c
+++ C/Sorting/Shell_sort.c
@@ -1,16 +1,19 @@
+/*Like Insertion sort elements are inserted in shell sort but in shell sort they are distance apart(there is a gap in between the elements) 
+by which sorting is performed. Here in shell sort we will not sort consecutive elements but the elements with a gap(distance) of calculated value(no. of elements/2)*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
 void ShellSort(int A[],int n)
 {
     int i,j,gap;int temp;
-    for(gap=n/2;gap>=1;gap/=2)
+    for(gap=n/2;gap>=1;gap/=2) // Performing the sorting by reducing value of gap until it is greater than equal to 1
     {
         for(i=gap;i<n;i++)
         {
-            temp=A[i];
+            temp=A[i]; //Element that we want to insert
             j=i-gap;
-            while(j>=0 && A[j]>temp)//shifting elements
+            while(j>=0 && A[j]>temp)// for shifting elements
             {
                 A[j+gap]=A[j];
                 j=j-gap;
@@ -22,7 +25,7 @@ void ShellSort(int A[],int n)
 }
 int main()
 {
-    int A[10],n=10,i;
+    int A[10],n=10,i; // n denotes the number of elements
     printf("Enter elements to be sorted using shell sort\n");
     for(int i=0;i<n;i++)
     {
