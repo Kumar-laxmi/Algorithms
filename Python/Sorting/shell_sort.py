28773069d1ed46fd9173ddae0b5d8565aa1e6364
Kinshuk Goyal <72309507+Kinshuk2003@users.noreply.github.com>
2023-01-11 10:38:23 +0530

    updated with requested changes

--- Python/Sorting/shell_sort.py
+++ Python/Sorting/shell_sort.py
@@ -1,17 +1,21 @@
+# This Algorithm is modification over the Insertion Sort
+# uses a gap value (greater than 1) to move elements to ahead
+
 def shell_Sort(arr):
     n= len(arr)  # size of array
     # define the gap
     gap = n//2
 
-    while gap > 0:  # loop will run until gap becomes 1
-                  # gap = 1, shell sort becomes insertion sort  
+    while gap > 0:  # loop will run until gap becomes 0
+                  # gap == 1, shell sort becomes insertion sort  
         j = gap
         # Check the array from left to right
-        
-        while j < n:
+        # till last index of j
+        while j < n:  
             i = j-gap 
               
             while i >= 0:
+                #checking values at index i and (i+gap)
                 # If value on right side is already greater than left side value
                 # We don't have to do swap else we will swap
                 if arr[i+gap] > arr[i]:
@@ -20,10 +24,10 @@ def shell_Sort(arr):
                 else:
                     arr[i+gap],arr[i] = arr[i],arr[i+gap]
   
-                i = i-gap # To check left side also
-            j +=1
+                i = i-gap  #To check element on left side also
+            j +=1 
 
-        gap = gap//2 #reducing the gap by half 
+        gap = gap//2 #reducing the gap by half in subsequent iterations
 
 #main function
 arr = [12, 34, 54, 2, 3, 7, 9]
