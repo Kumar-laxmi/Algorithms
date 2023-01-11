bb90342486d85611af9cc4c63ad2e35193d343d5
Sachit <sachittarway2002@gmail.com>
2023-01-11 22:41:12 +0530

    Comments added || Sieve of Erathenes || #173

--- C/Maths/sieveOfEratosthenes.c
+++ C/Maths/sieveOfEratosthenes.c
@@ -1,4 +1,7 @@
-// Sieve of Eratosthenes
+/*Sieve of Eratosthenes
+  In this method first we create an array of n number and then we start traversing it and the numbers 
+  which encounters its multiple is marked and the left out numbers are prime numbers*/
+
 #include<stdio.h>
 
 int main(){
@@ -8,7 +11,7 @@ int main(){
 
     int arr[10000]={0};
     for(int i=2;i<=n;i++){
-        if(arr[i]==0){
+        if(arr[i]==0){         //Markiong the numbers
             for(int j=i*i;j<=n;j+=i){
                 arr[j]=1;
             }
