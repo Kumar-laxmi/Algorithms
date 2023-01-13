d825d5316400f608c99a16f46557d2c2b3cb7596
linneszyx <itssuryanshcooldude346@gmail.com>
2023-01-14 01:45:52 +0530

    refactor nqueen

--- C++/Backtracking/nqueen.cpp
+++ C++/Backtracking/nqueen.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
-
 bool canPlace(int board[][20], int n, int x, int y)
 {
-
-    
-   /* Checking if there is a queen in the same column. */
+/* Checking if there is a queen in the same column. */
     for (int k = 0; k < x; k++)
     {
         if (board[k][y] == 1)
@@ -13,7 +10,6 @@ bool canPlace(int board[][20], int n, int x, int y)
             return false;
         }
     }
-    
    /* Checking if there is a queen in the left diagonal. */
     int i = x;
     int j = y;
@@ -26,8 +22,6 @@ bool canPlace(int board[][20], int n, int x, int y)
         i--;
         j--;
     }
-
-    
     /* Checking if there is a queen in the right diagonal. */
     i = x;
     j = y;
@@ -42,10 +36,8 @@ bool canPlace(int board[][20], int n, int x, int y)
     }
     return true;
 }
-
 void printBoard(int n, int board[][20])
 {
-
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -56,35 +48,33 @@ void printBoard(int n, int board[][20])
     }
     cout << endl;
 }
-
 /**
  * We are trying to place a queen in every row, and for every queen we place, we are trying to place
- * the next queen in the next row. 
- * 
- * If we are able to place all the queens, we have found a solution. 
- * 
+ * the next queen in the next row.
+ *
+ * If we are able to place all the queens, we have found a solution.
+ *
  * If we are not able to place all the queens, we backtrack and try to place the queen in the next
- * column. 
- * 
+ * column.
+ *
  * If we are not able to place the queen in any column, we backtrack and try to place the queen in the
- * next column in the previous row. 
- * 
+ * next column in the previous row.
+ *
  * If we are not able to place the queen in any column in the previous row, we backtrack and try to
- * place the queen in the next column in the previous row. 
- * 
+ * place the queen in the next column in the previous row.
+ *
  * And so on.
- * 
+ *
  * Args:
  *   n (int): The size of the board.
  *   board (int): The chess board.
  *   i (int): row number
- * 
+ *
  * Returns:
  *   a boolean value.
  */
 bool solveNQueen(int n, int board[][20], int i)
-{
-    // base case
+{  // base case
     if (i == n)
     {
         // Print the board
@@ -114,7 +104,7 @@ bool solveNQueen(int n, int board[][20], int i)
 /**
  * If the current queen can be placed safely in current column, mark this column as part of the
  * solution and recursively check if placing a queen in the next column leads to a solution
- * 
+ *
  * Returns:
  *   The number of solutions to the N-Queen problem.
  */
@@ -122,7 +112,7 @@ int main()
 {
     int board[20][20] = {0};
     int n;
-    cout<<"Number of Queens you want to place : ";
+    cout << "Number of Queens you want to place : ";
     cin >> n;
     solveNQueen(n, board, 0);
     return 0;
