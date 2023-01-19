2f3fbe2d1f907f2ea6f10139a622790ae09c9459
ADITYA0714 <95866052+ADITYA0714@users.noreply.github.com>
2023-01-19 09:33:07 +0530

    tower of hanoi
    I have uploaded the code of tower of hanoi.
    my issue is : #272

---
+++ C/Backtracking/towerofhanoi.c
@@ -0,0 +1,30 @@
+/* Name: Aditya kumar
+github username: aditya0714
+repositary name: algorithms
+issue no. - #272
+In tower of hanaoi rings/ disks are stack are one above the other placing the largest at bottom and increasing in order*/
+
+#include <stdio.h>
+ 
+void towers(int, char, char, char);
+ 
+int main()
+{
+    int number;
+    printf("Enter the number of rings : ");
+    scanf("%d", &number);
+    printf("Order of moves involve in the Tower of Hanoi are :\n");
+    towers(number, 'A', 'C', 'B');
+    return 0;
+}
+void towers(int number, char frompole, char topole, char auxpole)
+{
+    if (number == 1)
+    {
+        printf("\n Move disk 1 from pole %c to pole %c", frompole, topole);
+        return;
+    }
+     towers(number - 1, frompole, auxpole, topole);
+    printf("\n Move ring %d from pole %c to pole %c", number, frompole, topole);
+    towers(number - 1, auxpole, topole, frompole);
+}
