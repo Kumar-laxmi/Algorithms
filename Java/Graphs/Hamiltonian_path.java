62864c238df41ee7da6439181563fe8f9d62f454
Anurag Dutta <imanurag18@gmail.com>
2023-01-23 13:05:52 +0530

    Update Hamiltonian_path.java

--- Java/Graphs/Hamiltonian_path.java
+++ Java/Graphs/Hamiltonian_path.java
@@ -1,75 +1,101 @@
-import java.util.*;
-class Swoc
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+
+public class Hamiltonian_path
 {
-    public static void main(String args[])throws IOException
+    private int V, count;
+    private int[] path;     
+    private int[][] graph;
+ 
+    /** Function to find cycle **/
+    public void findHamiltonianCycle(int[][] g)
     {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while(t-- > 0)
+        V = g.length;
+        path = new int[V];
+ 
+        Arrays.fill(path, -1);
+        graph = g;        
+        try
+        {            
+            path[0] = 0;
+            count = 1;            
+            solve(0);
+            System.out.println("No solution");
+        }
+        catch (Exception e)
         {
-            String input_line[] = read.readLine().trim().split("\\s+");
-            int N = Integer.parseInt(input_line[0]);
-            int M = Integer.parseInt(input_line[1]);
-
-            input_line = read.readLine().trim().split("\\s+");
-            ArrayList<ArrayList<Integer>> Edges = new ArrayList<ArrayList<Integer>>();
-            for(int i = 0;i < M;i++)
-            {
-                ArrayList<Integer> e = new ArrayList<Integer>();
-                e.add(Integer.parseInt(input_line[2*i]));
-                e.add(Integer.parseInt(input_line[2*i+1]));
-                Edges.add(e);
-            }
-            Solution ob = new Solution();
-            if(ob.check(N, M, Edges))
-                System.out.println(1);
-            else
-                System.out.println(0);
+            System.out.println(e.getMessage());
+            display();
         }
     }
-}
-
-class Hamiltonian_path 
-{ 
-    boolean check(int N, int M, ArrayList<ArrayList<Integer>> Edges) 
-    { 
-      ArrayList<Integer> al[]=new ArrayList[N+1];
+   
+    public void solve(int vertex) throws Exception
+    {
       
-      for(int i=0;i<N+1;i++)
-          al[i]=new ArrayList<>();
-      
-      for(ArrayList<Integer> ed:Edges)
-      {
-          int src=ed.get(0);
-          int des=ed.get(1);
-          al[src].add(des);
-          al[des].add(src);
-      }
-      Set<Integer> st=new HashSet<>();
-    
-     for(int i=1;i<=N;i++)
-     {
-     if(find(al,i,st))
-     return true;
-     }
-      return false;
-   }
-   public static boolean find(ArrayList<Integer> al[],int src,Set<Integer> st)
-   {
-               st.add(src);
-       if(st.size()==al.length-1)
-       return true;
-
-       for(Integer ele:al[src])
-       {
-           if(!st.contains(ele))
-           {
-               if(find(al,ele,st))
-               return true;
-           }
-       }
-       st.remove(src);
-       return false;
-   }
+        if (graph[vertex][0] == 1 && pathCount == V)
+            throw new Exception("Solution found");
+        
+        if (count == V)
+            return;
+ 
+        for (int v = 0; v < V; v++)
+        {
+            if (graph[vertex][v] == 1 )
+            {
+                /** add to path **/            
+                path[count++] = v;    
+                /** remove connection **/            
+                graph[vertex][v] = 0;
+                graph[v][vertex] = 0;
+ 
+                /** if vertex not already selected  solve recursively **/
+                if (!isPresent(v))
+                    solve(v);
+ 
+                /** restore connection **/
+                graph[vertex][v] = 1;
+                graph[v][vertex] = 1;
+                /** remove path **/
+                path[--count] = -1;                    
+            }
+        }
+    }    
+    /** function to check if path is already selected **/
+    public boolean isPresent(int v)
+    {
+        for (int i = 0; i < count - 1; i++)
+            if (path[i] == v)
+                return true;
+        return false;                
+    }
+    /** display solution **/
+    public void display()
+    {
+        System.out.print("\nPath : ");
+        for (int i = 0; i <= V; i++)
+            System.out.print(path[i % V] +" ");
+        System.out.println();
+    }    
+  
+    public static void main (String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("HamiltonianCycle Algorithm Test\n");
+   
+        HamiltonianCycle hc = new HamiltonianCycle();
+ 
+        /** Accept number of vertices **/
+        System.out.println("Enter number of vertices\n");
+        int V = scan.nextInt();
+ 
+        /** get graph **/
+        System.out.println("\nEnter matrix\n");
+        int[][] graph = new int[V][V];
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                graph[i][j] = scan.nextInt();
+ 
+        hc.findHamiltonianCycle(graph);        
+    }    
 }
