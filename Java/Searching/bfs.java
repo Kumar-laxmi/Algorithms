7acebfd5837553e7680d09ae1b0dc7c0edf2e08a
Mansi Bhayade <mansibhayade0804@gmail.com>
2023-01-26 22:24:12 +0530

    Added comments to the code and removed the .class file

--- Java/Searching/bfs.java
+++ Java/Searching/bfs.java
@@ -1,3 +1,5 @@
+// Java program to print BFS traversal of a graph with the help of adjancecy list
+
 import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
@@ -8,17 +10,18 @@ class bfs {
     static boolean[] visited;
     static LinkedList<Integer>[] adj;   // Intialization of Linked List
 
+    // Function used by BFS
     static void BFS_iterative(int v) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(v);
-        visited[v] = true;
-        while (!queue.isEmpty()) {
-            v = queue.poll();
-            System.out.print(v + " ");
-            for (Integer i : adj[v]) {
+        queue.add(v);    // Add all the given vertices into the queue
+        visited[v] = true;  // Set boolean value of the visited vertex as true
+        while (!queue.isEmpty()) {  // If queue is not empty
+            v = queue.poll();       // Remove the vertex from the front end of the queue and print it
+            System.out.print(v + " ");  
+            for (Integer i : adj[v]) {   
                 if (!visited[i]) {
                     visited[i] = true;
-                    queue.add(i);
+                    queue.add(i);  // add the vertex to the top of the queue if the vertex is not visited 
                 }
             }
         }
@@ -29,28 +32,28 @@ class bfs {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of vertices: ");
-        V = sc.nextInt();
+        V = sc.nextInt();       // Take input of number of vertices from the user 
         
         visited = new boolean[V];
         adj = new LinkedList[V];
         for (int i = 0; i < V; i++) {
-            adj[i] = new LinkedList<>();
+            adj[i] = new LinkedList<>();    // add vertices into the linked list
         }
 
         
         System.out.print("Enter the number of edges: ");
         E = sc.nextInt();
         System.out.println("Enter the edges in the format <to> <from> : ");
-         while (count <= E) 
+         while (count <= E)            // Loop to take input of edges of the graph in the form of adjancecy list
             {
                 to = sc.nextInt();
                 from = sc.nextInt();
  
-                adj[to].add(from);
+                adj[to].add(from);     // adding to to from list
                 count++;
             }
 
-        System.out.print("Enter the starting vertex: ");
+        System.out.print("Enter the starting vertex: ");  // Determines the source vertex for BFS traversal
         int start = sc.nextInt();
 
         BFS_iterative(start);
