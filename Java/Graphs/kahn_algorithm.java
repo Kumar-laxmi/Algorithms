1f87e97420c2195c64a0267e1a2819452ed3eefd
Kapil Soni <kapilsoni54768161@gmail.com>
2023-01-26 10:17:48 +0530

    Added the Kahn's Algorithm in c++ and java

--- Java/Graphs/kahn_algorithm.java
+++ Java/Graphs/kahn_algorithm.java
@@ -21,7 +21,6 @@
 import java.util.*;
 
 class Solution{
-
     // Function to return list containing vertices in Topological order.
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
         int indegree[] = new int[V];
@@ -31,7 +30,6 @@ class Solution{
             }
         }
         Queue<Integer> q = new LinkedList<Integer>();
-        ;
         for (int i = 0; i < V; i++) {
             if (indegree[i] == 0) {
                 q.add(i);
