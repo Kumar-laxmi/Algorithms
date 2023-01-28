f3d2ade71ec4de7bb286d3fecdecbbf78fc545eb
shauryabaijal <103757677+shauryabaijal@users.noreply.github.com>
2023-01-28 23:55:36 +0530

    Update radixSort.py
    Added comments in the program

--- Python/radixSort.py
+++ Python/radixSort.py
@@ -1,13 +1,17 @@
+# Sorting the components based on significant places using counting sort(csort function)
 def csort(ip_array, i, arr_len):
     output = [0] * (arr_len)
     temp = [0] * (10)
+    # Calculate count of elements
     for ele in range(0, arr_len):
         idx = (ip_array[ele]/i)
         temp[int((idx)%10)] += 1
 
+# Calculate cumulative count(from element in range(1,10))
     for ele in range(1,10):
         temp[ele] += temp[ele-1]
 
+ # Place the elements in sorted order
     ele = arr_len-1
     while ele >= 0:
         idx = (ip_array[ele]/i)
@@ -19,8 +23,12 @@ def csort(ip_array, i, arr_len):
     ele = 0
     for ele in range(0,len(ip_array)):
         ip_array[ele] = output[ele]
+        
+# Main function to implement radix sort
 def sort(ip_array):
+# Get maximum element
     max_ele = max(ip_array)
+    # Use the counting sort to arrange the elements according to place value.
     i = 1
     while max_ele/i > 0:
         csort(ip_array,i, len(ip_array))
