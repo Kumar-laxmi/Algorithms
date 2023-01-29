e0a2dc1b1b6bdda2df9f94b95550b0dc37c89e33
MrKoshens <madhavvermmakeshav@gmail.com>
2023-01-29 12:11:09 +0530

    RSA algorithm. I will make a new pull request for job sequencing

--- Python/Greedy-Algorithm/jobSequencing.py
+++ Python/Greedy-Algorithm/jobSequencing.py
@@ -1,27 +1,34 @@
+# Job Sequencing Problem (Greedy Algorithm)
 
+jobs = []
+num_of_jobs = int(input("Enter the number of jobs: "))
 
-def schedule_jobs(jobs):
-    n = len(jobs)
-    # Sort jobs in descending order of profit per unit time
-    jobs.sort(key=lambda x: x[2]/x[1], reverse=True)
-    schedule = [0] * n
-    total_profit = 0
-    for i in range(n):
-        for j in range(min(n, jobs[i][1]) - 1, -1, -1):
-            if schedule[j] == 0:
-                schedule[j] = i + 1
-                total_profit += jobs[i][2]
-                break
-    return schedule, total_profit
+for i in range(num_of_jobs):
+    job_id = int(input("Enter job id: "))
+    job_deadline = int(input("Enter job deadline: "))
+    job_profit = int(input("Enter job profit: "))
+    jobs.append((job_id, job_deadline, job_profit))
 
-# Sample usage
-job_count = int(input("Enter the number of jobs: "))
-jobs = []``
-for i in range(job_count):
-    job_name = input("Enter the name of job {}: ".format(i+1))
-    deadline = int(input("Enter the deadline of job {}: ".format(i+1)))
-    profit = int(input("Enter the profit of job {}: ".format(i+1)))
-    jobs.append([job_name, deadline, profit])
-schedule, profit = schedule_jobs(jobs)
-print("Optimal Schedule: ", schedule)
-print("Maximum Profit: ", profit)
+# Sort jobs in descending order of profit
+jobs = sorted(jobs, key=lambda x: x[2], reverse=True)
+
+# Initialize an array to keep track of the deadline of each job
+job_slots = [False] * num_of_jobs
+
+# Initialize a variable to keep track of the total profit
+total_profit = 0
+
+# Iterate through each job
+for job in jobs:
+    for i in range(min(job[1]-1, num_of_jobs-1), -1, -1):
+        if not job_slots[i]:
+            job_slots[i] = True
+            total_profit += job[2]
+            break
+
+print("Optimal Sequence of Jobs:")
+for i in range(num_of_jobs):
+    if job_slots[i]:
+        print(jobs[i][0])
+
+print("Maximum Profit: ", total_profit)
