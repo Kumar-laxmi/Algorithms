49a8cf71f3348254d299591f52d873126a985cc1
Srishti Majumder <srishtimajumder2002@gmail.com>
2023-02-03 20:10:33 +0530

    Added all the comments

--- C++/Maths/KNN.cpp
+++ C++/Maths/KNN.cpp
@@ -5,10 +5,12 @@ using namespace std;
 #include<conio.h>
 #include<math.h>
 
+//function of square 
 int square(int n){
   return n*n;
 }
 
+//function to sort an array in ascending
 void asc_sort(int a[10], int n)
 {
  int i, j, temp;
@@ -28,52 +30,56 @@ void asc_sort(int a[10], int n)
 
 int main()
 {
-  int maths[] = {56, 78, 23, 19, 20, 45, 90};
-  int Eng[] = {67, 34, 23, 22, 14, 68, 99};
-  int Science[] = {12, 31, 44, 56, 67, 78,10};
-  char Result[12][10] = {"PASS", "PASS","FAIL","FAIL","FAIL","PASS","PASS"};
+  int maths[] = {56, 78, 23, 19, 20, 45, 90}; //marks of maths
+  int Eng[] = {67, 34, 23, 22, 14, 68, 99}; //marks of English
+  int Science[] = {12, 31, 44, 56, 67, 78,10}; //marks of sceince
+  char Result[12][10] = {"PASS", "PASS","FAIL","FAIL","FAIL","PASS","PASS"}; //the remarks according to the marks of the subjects
 
+
+  //display the set of marks and remarks of all the subjects
   cout << "\n Initial Set:";
   cout << "\nMaths\tEng\tScinece\tResults";
   for (int i = 0; i < 7; i++) {
     cout << "\n" << maths[i] << "\t" << Eng[i] << "\t" << Science[i] << "\t" <<Result[i];
   }
 
-  int m,e,s,k;
+  // queries -- m for Maths, e for english, s for science and threshhold k
+  int m,e,s,k; 
   cout<<"\nMention the query(maths Eng Science):";
   cin>>m>>e>>s;
   cout<<"\nMention the threshhold:";
   cin>>k;
 
-  int dis[7];
-  int tab[7];
+  int dis[7]; //array consisting euclidian distances between query and all subject set
+  int tab[7]; //to store the copy of the dis array
   for(int j=0; j<7;j++)
   {
     dis[j] = sqrt(square(m-maths[j]) + square(e - Eng[j]) + square(s - Science[j]));
-    tab[j] = dis[j];
+    tab[j] = dis[j]; //copying the elements of dis array i tab array
   }
 
-  asc_sort(tab,7);
+  asc_sort(tab,7); //calling the function to sort tab array in ascending order
 
-  int nof=0, nop=0;
+  int nof=0, nop=0; // nof --- number of FAIL(s) , nop --- number of PASS(s)
   cout << "\nMaths\tEng\tScinece\tResults";
   for(int l = 0; l < k; l++) {
     for(int n = 0; n <7; n++)
     {
-      if(tab[l] == dis[n])
+      if(tab[l] == dis[n]) // compare the two arrays to find the common index number(n)
       {
         cout << "\n" << maths[n] << "\t" << Eng[n] << "\t" << Science[n] << "\t" <<Result[n];
         if (strcmp(Result[n], "FAIL") == 0) {
-          nof++;
+          nof++; //if the Result(n) consists of FAIL then increase fail count by 1
         }
         if (strcmp(Result[n], "PASS") == 0) {
-          nop++;
+          nop++; //if the Result(n) consists of PASS then increase pass count by 1
         }
       }
     }
   }
 
-  if (nof > nop) {
+  //compare the numbers of pass and fail to declare the classification of the tuple
+  if (nof > nop) { 
     cout << "\n New Tuple is classified as FAIL";
   }
   else{
@@ -84,134 +90,3 @@ int main()
 }
 
 
-
-
-
-
- 
-// void main() {
-//   int i, j, l;
-//   int tsc = 12;
-//   char gen[12] = {
-//     'F',
-//     'M',
-//     'F',
-//     'F',
-//     'F',
-//     'M',
-//     'F',
-//     'M',
-//     'M',
-//     'F',
-//     'M',
-//     'F'
-//   };
-//   float h[12] = {
-//     1.6f,
-//     2.0f,
-//     1.9f,
-//     1.88f,
-//     1.7f,
-//     1.85f,
-//     1.6f,
-//     1.7f,
-//     2.2f,
-//     1.8f,
-//     1.95f,
-//     1.9f
-//   };
-  // char op[12][10] = {
-  //   "short",
-  //   "tall",
-  //   "medium",
-  //   "medium",
-  //   "short",
-  //   "medium",
-  //   "short",
-  //   "short",
-  //   "tall",
-  //   "medium",
-  //   "medium",
-  //   "medium"
-  // };
- 
-//   cout << "\n Initial Set:";
-//   cout << "\nGender\tHeight\tOutput";
-//   for (i = 0; i < 12; i++) {
-//     cout << "\n" << gen[i] << "\t" << h[i] << "\t" << op[i];
-//   }
- 
-//   float nh;
-//   char ng;
-//   cout << "\n Enter tuple to be processed (Height,Gender) :";
-//   cin >> nh >> ng;
- 
-//   int t;
-//   cout << "\n Enter threshold:";
-//   cin >> t;
- 
-//   float d[12][2], k;
- 
-//   //calculating distance to each value in training set
-//   for (i = 0; i < 12; i++) {
-//     d[i][0] = i;
-//     k = h[i] - nh;
-//     if (k < 0) {
-//       d[i][1] = -k;
-//     } else {
-//       d[i][1] = k;
-//     }
-//   }
- 
-//   //Sorting
-//   for (i = 0; i < 11; i++) {
-//     for (j = 0; j < 11; j++) {
-//       if (d[j][1] > d[j + 1][1]) {
-//         k = d[j][1];
-//         d[j][1] = d[j + 1][1];
-//         d[j + 1][1] = k;
- 
-//         l = d[j][0];
-//         d[j][0] = d[j + 1][0];
-//         d[j + 1][0] = l;
-//       }
-//     }
-//   }
- 
-//   int nos = 0; //no of shorts
-//   int nom = 0; //no of mediums
-//   int not = 0; //no of talls
- 
-//   cout << "\nGender\tHeight\tOutput\n";
-//   for (i = 0; i < t; i++) {
-//     l = d[i][0];
-//     cout << gen[l] << "\t" << h[l] << "\t" << op[l] << "\n";
-//     if (strcmp(op[l], "short") == 0) {
-//       nos++;
-//     }
-//     if (strcmp(op[l], "medium") == 0) {
-//       nom++;
-//     }
-//     if (strcmp(op[l], "tall") == 0) {
-//       not++;
-//     }
-//   }
- 
-//   cout << "\n No of shorts:" << nos;
-//   cout << "\n No of medium:" << nom;
-//   cout << "\n No of tall:" << not;
- 
-//   if (nos > nom && nos > not) {
-//     cout << "\n New Tuple is classified as Short";
-//   }
- 
-//   if (nom > nos && nom > not) {
-//     cout << "\n New Tuple is classified as Medium";
-//   }
- 
-//   if (not > nom && not > nos) {
-//     cout << "\n New Tuple is classified as Tall";
-//   }
- 
-//   getch();
-// }
