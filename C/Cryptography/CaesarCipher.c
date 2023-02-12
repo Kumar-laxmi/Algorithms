4671f0c72d8c335fdec98e932815e08a56f72c0e
MrKoshens <madhavvermmakeshav@gmail.com>
2023-02-12 23:15:34 +0530

    Algorithm added in C

--- C/Cryptography/CaesarCipher.c
+++ C/Cryptography/CaesarCipher.c
@@ -3,6 +3,26 @@
  with a letter shifted a certain number of places down the alphabet. For example, with a 
  shift of 1, A would be replaced by B, B would become C, and so on
  
+Algorithm:
+ Choose a key: The key is a positive integer that determines the number of positions each letter will be shifted in the alphabet.
+
+ Convert the plaintext message to a sequence of numbers: Each letter in the message is converted to its corresponding ASCII code 
+ or Unicode value.
+
+ Shift each letter: The value of each letter is shifted by the key. For example, if the key is 3 and the letter 'A' is being 
+ encrypted, its value will be shifted to 'D'.
+
+ Modulo 26: If the shifted value exceeds the value of 'Z' or 'z' (for uppercase and lowercase letters, respectively), the result 
+ is taken modulo 26 to wrap it back to the alphabet. For example, if the shifted value for 'Z' with a key of 3 is ']', the 
+ modulo 26 result will be 'C'.
+
+ Convert the sequence of shifted values back to letters: The shifted values are converted back to letters using their corresponding 
+ ASCII code or Unicode value.
+
+ The final output is the ciphertext: The resulting sequence of letters is the encrypted ciphertext message.
+
+ To decrypt the message, the process is reversed. The key used for decryption must be the same as the key used for encryption.
+ 
 Input: 
  Opton: First the code asks for Encryption or decryption. 
  Plain text: On choosing encrypt the code asks for plaintext.
