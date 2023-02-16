5042ec94a40fd1b3fe4b6d59df25c3deba4bbb93
Abhilash S  Bharadwaj <72621930+Abhilash1781@users.noreply.github.com>
2023-02-16 19:19:39 +0530

    Update Diffie_Hellman.py

--- Python/Cryptography/Diffie_Hellman.py
+++ Python/Cryptography/Diffie_Hellman.py
@@ -3,6 +3,8 @@ The Diffie-Hellman algorithm is being used to establish a shared secret that
 can be used for secret communications while exchanging data over a public network 
 using the elliptic curve to generate points and get the secret key using the parameters.
 I took same example of Alice and Bob which is mentioned in the issue . 
+Time complexity using single key exchange - O (n^3).
+Space Complexity - O (n)
 """
 
 
