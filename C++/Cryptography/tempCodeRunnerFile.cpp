f2f6a05482350b78a70c3c0c96c00873df544eaf
Paritosh-J <paritoshj2001@gmail.com>
2023-02-17 21:58:13 +0530

    Vernam Cipher

--- C++/Cryptography/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-andom key from 26 alphabetic characters.
-⇒ Assign a number to each character of the plain-text and the key according to alphabetical order.
-⇒ Add the assigned numbers and store the same in a new array/vector.
-⇒ Subtract 26 from the added values, if they are greater than 26.
-⇒ Assign the characters 
