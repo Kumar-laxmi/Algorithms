cad059de532c2f4e363db13f6290d1aaef37260d
Atul <atul.ak1510@gmail.com>
2023-02-19 02:32:31 +0530

    same changes in C and python language

--- Python/Sorting/Triplet.py
+++ Python/Sorting/Triplet.py
@@ -9,29 +9,36 @@
 #1] Input: array = { 7, 10, 6, 14, 4, 9 }, sum = 25;
 #Output: Triplet is 4, 7, 14 */
 # Python program to find a triplet
-def Triplet(A, arr_size, sum):
-	# Sort the elements
-	A.sort()
-	# fix the first element and one by one and find the other two elements
-	for i in range(0, arr_size-2):
-		# To find the other two elements, start two index variables from two corners of the array and
-		# move them toward each other index of the first element in the remaining elements
-		x = i + 1
-		# index of the last element
-		y = arr_size-1
-		while (x < y):
-			if( A[i] + A[x] + A[y] == sum):
-				print("Triplet is", A[i],
-					', ', A[x], ', ', A[y])
-				return True
-			elif (A[i] + A[x] + A[y] < sum):
-				x += 1
-			else:
-				y -= 1
-	# no triplet was found
-	return False
-# Main Function
-A = [ 7, 10, 6, 14, 4, 9 ]
-sum = 25
-arr_size = len(A)
-Triplet(A, arr_size, sum)
+def find_triplet(A, arr_size, sum):
+    # Sort the elements of the array in ascending order
+    A.sort()
+    # Fix the first element of the triplet and find all other possible elements
+    for i in range(arr_size - 2):
+        # For the other two elements, start two index variables from two ends of the array and move them toward each other
+        x = i + 1 # index of the first element of the remaining elements
+        y = arr_size - 1 # index of the last element
+        while x < y:
+            # If the triplet's sum equals the given sum, print it and return True
+            if A[i] + A[x] + A[y] == sum:
+                print("Triplet is {}, {}, {}".format(A[i], A[x], A[y]))
+                return True
+            # If the triplet's sum is less than the given sum, move the left pointer to the right
+            elif A[i] + A[x] + A[y] < sum:
+                x += 1
+            # If the triplet's sum is greater than the given sum, move the right pointer to the left
+            else:
+                y -= 1
+    # If no triplet is found, return False
+    return False
+# Main function
+if __name__ == "__main__":
+    n = int(input("Enter the size of the array: "))
+    A = []
+    print("Enter {} integers in the array:".format(n))
+    for i in range(n):
+        A.append(int(input()))
+    sum = int(input("Enter the sum value: "))
+    arr_size = len(A)
+    found = find_triplet(A, arr_size, sum)
+    if not found:
+        print("No triplet found with the given sum.")
