15860445ed61b59cb1c755a8e888881c5ba1c93f
Atul <atul.ak1510@gmail.com>
2023-02-19 03:01:04 +0530

    modify the code so it take input from the user

--- C/Trees/Symmetric_Tree.c
+++ C/Trees/Symmetric_Tree.c
@@ -19,57 +19,69 @@ OUTPUT: Symmetric
 3 3
 OUTPUT: Not symmetric */
 // C program to check if a given Binary Tree is symmetric or not
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 // Construct a Binary Tree Node
-typedef struct Node {
-	int key;
-	struct Node *left, *right;
-} Node;
-// function to create a new Node
-Node* newNode(int key)
+struct Node 
 {
-	Node* temp = (Node *)malloc(sizeof(Node));
-	temp->key = key;
-	temp->left = temp->right = NULL;
-	return (temp);
+    int key;
+    struct Node* left, *right;
+};
+// function to create a new Node
+struct Node* newNode(int key)
+{
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->key = key;
+    temp->left = temp->right = NULL;
+    return (temp);
 }
 // Returns true if trees with roots as childL and childR are mirror image of each other
-bool isSymmertic(Node* root1, Node* root2)
+int isSymmetric(struct Node* root1, struct Node* root2)
 {
-	// If both trees are empty, then they are mirror images
-	if (root1 == NULL && root2 == NULL)
-		return true;
-	// For two trees to be mirror images, the following 3 conditions must be true
-	// a) Their root node's key must be same
-	// b) left subtree of left tree and right subtree of right tree have to be mirror images
-	// c) right subtree of left tree and left subtree of right tree have to be mirror images
-	if (root1 && root2 && root1->key == root2->key)
-		return isSymmertic(root1->left, root2->right)
-			&& isSymmertic(root1->right, root2->left);
-	// Returns true if a tree is symmetric.
-	return false;
+    // If both trees are empty, then they are mirror images
+    if (root1 == NULL && root2 == NULL)
+        return 1;
+    // For two trees to be mirror images, the following 3 conditions must be true
+    // a) Their root node's key must be same
+    // b) left subtree of left tree and right subtree of right tree have to be mirror images
+    // c) right subtree of left tree and left subtree of right tree have to be mirror images
+    if (root1 && root2 && root1->key == root2->key)
+        return isSymmetric(root1->left, root2->right)
+            && isSymmetric(root1->right, root2->left);
+    // if none of above conditions is true then childL and childR are not mirror images
+    return 0;
 }
-// Check if tree is mirror of itself
-bool isSimilar(Node* root)
+// Returns true if a tree is symmetric.
+int isSimilar(struct Node* root)
 {
-	return isSymmertic(root, root);
+    // Check if tree is mirror of itself
+    return isSymmetric(root, root);
+}
+// Function to take input from user
+struct Node* takeInput() 
+{
+    int val;
+    printf("Enter root value (-1 for no node): ");
+    scanf("%d", &val);
+    if (val == -1) {
+        return NULL;
+    }
+    struct Node* root = newNode(val);
+    root->left = takeInput();
+    root->right = takeInput();
+    return root;
 }
 // Main function
 int main()
 {
-	//Construct the given Tree
-	Node* root = newNode(1);
-	root->left = newNode(2);
-	root->right = newNode(2);
-	root->left->left = newNode(3);
-	root->left->right = newNode(4);
-	root->right->left = newNode(4);
-	root->right->right = newNode(3);
-	if (isSimilar(root))
-		printf("Is Symmetric");
-	else
-		printf("Is Not symmetric");
-	return 0;
+    // Take input for the binary tree
+    printf("Enter elements of the binary tree:\n");
+    struct Node* root = takeInput();
+    if (isSimilar(root)) {
+        printf("The tree is symmetric");
+    }
+    else {
+        printf("The tree is not symmetric");
+    }
+    return 0;
 }
--- Java/Trees/Symmetric_Tree.java
+++ Java/Trees/Symmetric_Tree.java
@@ -18,47 +18,71 @@ OUTPUT: Symmetric
 \
 3 3
 OUTPUT: Not symmetric */
-
-public class Symmetric_Tree {
-	Node root;
-	// Returns true if trees with roots as childL and childR are mirror image of each other
-	boolean isMirror(Node node1, Node node2)
+import java.util.*;
+class Node 
+{
+    int key;
+    Node left, right;
+}
+public class Symmetric_Tree 
+{
+    // Function to create a new node
+    public static Node newNode(int key) 
 	{
-	// If both trees are empty, then they are mirror images
-		if (node1 == null && node2 == null)
-			return true;
-	// For two trees to be mirror images, the following 3 conditions must be true
-	// a) Their root node's key must be same
-	// b) left subtree of left tree and right subtree of right tree have to be mirror images
-	// c) right subtree of left tree and left subtree of right tree have to be mirror images
-		if (node1 != null && node2 != null
-			&& node1.key == node2.key)
-			return (isMirror(node1.left, node2.right)
-					&& isMirror(node1.right, node2.left));
-	// if none of above conditions is true then childL and childR are not mirror images
-		return false;
-	}
-	// Returns true if a tree is symmetric.
-	boolean isSymmetric()
+        Node temp = new Node();
+        temp.key = key;
+        temp.left = temp.right = null;
+        return temp;
+    }
+    // Returns true if trees with roots as childL and childR are mirror image of each other
+    public static boolean isSymmetric(Node root1, Node root2) 
 	{
-	// Check if tree is mirror of itself
-		return isMirror(root, root);
-	}
-	// Main Function
-	public static void main(String args[])
+        // If both trees are empty, then they are mirror images
+        if (root1 == null && root2 == null) 
+		{
+            return true;
+        }
+        // For two trees to be mirror images, the following 3 conditions must be true
+        // a) Their root node's key must be same
+        // b) left subtree of left tree and right subtree of right tree have to be mirror images
+        // c) right subtree of left tree and left subtree of right tree have to be mirror images
+        if (root1 != null && root2 != null && root1.key == root2.key) 
+		{
+            return isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+        }
+        // if none of above conditions is true then childL and childR are not mirror images
+        return false;
+    }
+    // Returns true if a tree is symmetric.
+    public static boolean isSimilar(Node root) 
 	{
-		Symmetric_Tree tree = new Symmetric_Tree();
-		tree.root = new Node(1);
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(2);
-		tree.root.left.left = new Node(3);
-		tree.root.left.right = new Node(4);
-		tree.root.right.left = new Node(4);
-		tree.root.right.right = new Node(3);
-		boolean output = tree.isSymmetric();
-		if (output == true)
-			System.out.println("Is Symmetric");
-		else
-			System.out.println("Is Not symmetric");
-	}
+        // Check if tree is mirror of itself
+        return isSymmetric(root, root);
+    }
+    // Function to take input from user
+    public static Node takeInput(Scanner sc) 
+	{
+        System.out.print("Enter root value (-1 for no node): ");
+        int val = sc.nextInt();
+        if (val == -1) {
+            return null;
+        }
+        Node root = newNode(val);
+        root.left = takeInput(sc);
+        root.right = takeInput(sc);
+        return root;
+    }
+    // Main function
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        // Take input for the binary tree
+        System.out.println("Enter elements of the binary tree:");
+        Node root = takeInput(sc);
+        if (isSimilar(root)) {
+            System.out.println("The tree is symmetric");
+        } else {
+            System.out.println("The tree is not symmetric");
+        }
+    }
 }
