ee72d9b8936d0ab5f1319c8f20816e27c92dbf90
Palak Singhania <singhaniapalak2002@gmail.com>
2023-02-20 11:36:22 +0530

    Update PerfectNumber.java

--- Java/Maths/PerfectNumber.java
+++ Java/Maths/PerfectNumber.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class PerfectNumber {
     public static void main(String[] args) {
+        // take user input 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int N = sc.nextInt();
+        
+        // initialize variable for loop and to store sum 
         int i, sum = 0;
+        
+        // iterate from 1 to N and if the number is a factor of N then add it to sum
         for (i = 1; i < N; i++) {
             if (N % i == 0) {
                 sum += i;
             }
         }
+        
+        // if sum of factors of N is equal to N then it is a perfect number
         if (sum == N) {
             System.out.print(N + " is a Perfect Number\n");
         } else {
