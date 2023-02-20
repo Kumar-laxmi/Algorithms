136f391d6fd9d3b82c3c0ee393b0a6c6fb128cd3
Palak Singhania <singhaniapalak2002@gmail.com>
2023-02-20 12:07:38 +0530

    Update perfect.cpp

--- C++/Maths/perfect.cpp
+++ C++/Maths/perfect.cpp
@@ -5,15 +5,22 @@ using namespace std;
 
 int main ()
 {  
-    int i, num, div, sum=0;
+    // initialize variable for loop, input number and to store sum 
+    int i, num, sum=0;
+    
+    // take user input 
     cout << "Enter the number: ";
     cin >> num;
+    
+    // iterate from 1 to N and if the number is a factor of N then add it to sum
     for (i=1; i < num; i++)
     {
-        div = num % i;
+        int div = num % i;
         if (div == 0)
             sum = sum + i;
     }
+    
+    // if sum of factors of N is equal to N then it is a perfect number
     if (sum == num)
         cout << "\n" << num <<" is a perfect number";
     else
