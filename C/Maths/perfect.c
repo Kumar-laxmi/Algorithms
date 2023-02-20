a0dc23d76c51bdf2f2a66578ae4280bb73bb5e51
Palak Singhania <singhaniapalak2002@gmail.com>
2023-02-20 11:51:55 +0530

    Update perfect.c

--- C/Maths/perfect.c
+++ C/Maths/perfect.c
@@ -3,14 +3,22 @@
 #include<stdio.h>
 
 int main() {
+   
+    // initialize variable for loop, input number and to store sum 
     int i, num, sum = 0;
+    
+    // take user input 
     printf("\nEnter a number : ");
     scanf("%d", &num);
+    
+    // iterate from 1 to N and if the number is a factor of N then add it to sum
     for(i=1; i<num; i++) {
         if(num%i == 0) {
             sum += i;
         }
     }
+    
+    // if sum of factors of N is equal to N then it is a perfect number
     if(sum == num) {
         printf("\n%d is a Perfect Number\n", num);
     } else {
