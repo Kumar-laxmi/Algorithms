ff656748739e9f752be54ee2b9a39844e9847c5c
Palak Singhania <singhaniapalak2002@gmail.com>
2023-02-20 11:38:18 +0530

    Update perfect.py

--- Python/Maths/perfect.py
+++ Python/Maths/perfect.py
@@ -1,10 +1,17 @@
 # Python program to check if the number is a Perfect number or not
 
+# take user input 
 num = int(input("Enter a number : "))
+
+# initialize variable for loop and to store sum 
 sum = 0
+
+# iterate from 1 to N and if the number is a factor of N then add it to sum
 for i in range (1,num):
     if(num % i == 0):
         sum = sum + i
+        
+# if sum of factors of N is equal to N then it is a perfect number
 if(sum == num):
     print(num," is a perfect number")
 else:
