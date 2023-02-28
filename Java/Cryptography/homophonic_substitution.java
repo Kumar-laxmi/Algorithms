7b301933d5d383c0c9b943b3d4190b6ad3f7ac1e
MrKoshens <madhavvermmakeshav@gmail.com>
2023-03-01 00:46:17 +0530

    java code for homophonic substitution

--- Java/Cryptography/homophonic_substitution.java
+++ Java/Cryptography/homophonic_substitution.java
@@ -1,3 +1,28 @@
+/*
+Algorithm:
+    1. Define a substitution dictionary: In this step, the algorithm defines a dictionary 
+    that maps each letter in the plaintext message to one or more substitute characters 
+    in the ciphertext message.
+
+    2. Prompt for plaintext input: The algorithm prompts the user to enter the plaintext 
+    message that they want to encrypt.
+
+    3. Apply the substitution: The algorithm applies the substitution rules defined in 
+    the dictionary to each letter in the plaintext message. If a letter has multiple 
+    possible substitutes, one is chosen at random.
+
+    4. Generate the ciphertext: The resulting substitute characters are concatenated to 
+    form the ciphertext message.
+
+    5. Output the ciphertext: The algorithm outputs the resulting ciphertext to the user.
+
+Input:
+    A string which is also called a plain text.
+Output:
+    A string called cipher text which changes everytime the same plaintext is entered.
+    It substitutes the value of letters randomly to make the cipher text.
+*/
+
 import java.util.Scanner;
 
 public class HomophonicSubstitution {
