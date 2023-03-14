b61c957dab00f8876e024cce6f6772107b043320
Harika Raja <80022302+Harikaraja@users.noreply.github.com>
2023-03-14 18:56:03 +0530

    Update perceptron.py

--- Python/Deep learning/perceptron learning/perceptron.py
+++ Python/Deep learning/perceptron learning/perceptron.py
@@ -1,3 +1,23 @@
+'''
+# PERCEPTRON LEARNING BOLEAN AND , OR FUNCTIONS
+
+A Perceptron is an Artificial Neuron. It is the simplest possible Neural Network. Neural Networks are the building blocks of Machine Learning.
+
+![image](https://user-images.githubusercontent.com/80022302/223597791-3ef58982-9aca-4fde-9b0e-9da14bb88076.png)
+
+Perceptron can learn only linearly seperable data.
+
+Non linearly seperable data cannot be read by the perceptron.
+
+The data is given in the program using input() statement.
+
+The output obtained is:
+
+![image](https://user-images.githubusercontent.com/80022302/223598263-9bc239d7-6dcd-46bb-8b6a-91c3f0f0fdd1.png)
+
+'''
+
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
