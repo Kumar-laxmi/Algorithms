78893b048919eb92d1a9080b4d643415984055c5
Siddhant Tiwari <siddhant.tiwari.ds@gmail.com>
2023-03-15 20:49:49 +0530

    Fixed matrix initialization bug

--- Python/Dynamic-Programming/edit_distance.py
+++ Python/Dynamic-Programming/edit_distance.py
@@ -26,7 +26,7 @@ Author: Siddhant Tiwari (github.com/stiwari-ds)
 def count_operations(first_string: str, second_string: str) -> int:
     """Calculates the number of operations/edits required to convert first_string
     to second_string through insertion, removal or replacement of characters.
-    Dynamic programming approach: table/matrix of dimensions m x n is initialized,
+    Dynamic programming approach: table/matrix of dimensions m+1 x n+1 is initialized,
     where, m = length of first_string and n = length of second_string. The cells of
     the table denote sub-problems which represent conversion of a sub-string of the
     first_string to a sub-string of the second_string. Bottom-right cell of the table
@@ -41,16 +41,16 @@ def count_operations(first_string: str, second_string: str) -> int:
     """
     m = len(first_string)
     n = len(second_string)
-    dp_table = [[0] * n] * m  # m x n matrix
+    dp_table = [[0 for j in range(n + 1)] for i in range(m + 1)]  # m+1 x n+1 matrix
 
     for i in range(m + 1):
         for j in range(n + 1):
             if i == 0:  # first_string is empty
-                # no. of insertions required = length of second_string
+                # no. of insertions required = length of second string
                 dp_table[i][j] = j
 
             elif j == 0:  # second_string empty
-                # no. of insertions required = length of first_string
+                # no. of insertions required = length of first string
                 dp_table[i][j] = i
 
             elif first_string[i - 1] == second_string[j - 1]:  # last characters same
@@ -72,8 +72,8 @@ def edit_distance() -> None:
     to convert the first string to the second string. Edits can be in the form
     of insertion, removal or replacement of characters, one at a time.
     """
-    first_string = input("Enter first string: ").strip()
-    second_string = input("Enter second string: ").strip()
+    first_string = input("Enter first string: ").strip().lower()
+    second_string = input("Enter second string: ").strip().lower()
     num_edits = count_operations(first_string, second_string)
     print(f"\nNumber of edits required = {num_edits}")
 
