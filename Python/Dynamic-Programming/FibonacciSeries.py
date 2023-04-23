ddb6f2367a179715abaf09c45b8b34649b3ceaf1
Shubham jaiswar <118053732+shubhamjaiswar43@users.noreply.github.com>
2023-04-23 15:44:50 +0530

    Add files via upload

---
+++ Python/Dynamic-Programming/FibonacciSeries.py
@@ -0,0 +1,36 @@
+def fib(n, dp):
+    if n <= 0:
+        print(-1)
+        return -1
+    f1 = -1
+    f2 = -1
+    if n == 2:
+        print(f"{n-2} {n-1} ", end="")
+        dp[(n-2)] = (n-2)
+        dp[(n-1)] = (n-1)
+        return (n-1)
+    if n == 1:
+        print(f"{n-1} ")
+        dp[(n-1)] = (n-1)
+        return (n-1)
+    for i in range(n):
+        if dp[i] != -1 and i == (n-2):
+            f1 = dp[i]
+        if dp[i] != -1 and i == (n-3):
+            f2 = dp[i]
+    if f1 == -1:
+        f1 = fib(n-1, dp)
+        dp[n-2] = f1
+    for i in range(n):
+        if dp[i] != -1 and i == (n-2):
+            f1 = dp[i]
+        if dp[i] != -1 and i == (n-3):
+            f2 = dp[i]
+    dp[n-1] = (f1+f2)
+    print(f"{f1+f2} ", end="")
+    return (f1+f2)
+
+
+n = int(input("Enter a Number : "))
+dp = [-1 for i in range(n)]
+fib(n, dp)
