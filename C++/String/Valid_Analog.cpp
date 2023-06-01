19ff863733f5ffa8fe5f1bdea26cf463381e291a
shubhanshurav <shubhanshurav63@gmail.com>
2023-06-01 09:07:03 +0530

    Add Valid Analog Problem

--- C++/String/Valid_Analog.cpp
+++ C++/String/Valid_Analog.cpp
@@ -26,7 +26,6 @@ Time Complexity : O(m+n)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
         //make a frequency Table array of size 256 
         //and initialize all with 0
         int freqTable[256] = {0};
@@ -47,6 +46,35 @@ public:
         }
 
         return true;
+    }
+};
 
+
+// <----------- Approach - 02 ------------->
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //sort the both strings   
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        // is s.size() == t.size then 
+        // check each character of s with each character of t
+        // if both are equal than continue 
+        //other wise return false
+        if(s.size() == t.size()){
+            for(int i=0;i<s.size();i++){
+                if(s[i] == t[i]){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            return false;
+        }
+    //both strings are equal so we will return true
+    return true;
     }
 };
