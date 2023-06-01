e3a2d32b4c91c289f5c9c80db9b931ef9f5b9b9f
Sowmya Emani <sowmyaemani06@gmail.com>
2023-06-01 18:26:48 +0530

    dnf sort code added in c

---
+++ C/Sorting/dnf_sort.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void dnfSort(int arr[],int n){
+    int t1,t2;
+    int s = 0;
+    int mid = 0;
+    int e = n-1;
+    while(mid<=e){
+        if(arr[mid] == 0){
+            t1=arr[mid];
+            arr[mid]=arr[s];
+            arr[s]=t1;
+            s++;
+            mid++;
+        }
+        else if(arr[mid] == 1){ 
+            mid++;
+        }
+        else if(arr[mid] == 2){
+            t2=arr[mid];
+            arr[mid]=arr[e];
+            arr[e]=t2;
+            e--;
+        }
+    }
+    return;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[1000];
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d",&arr[i]);
+    }
+    dnfSort(arr, n);
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d",arr[i]);
+        printf(" ");
+    }
+    return 0;
+}
