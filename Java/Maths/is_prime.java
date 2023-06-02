300fee67f106d499a58a901a18d57e09bff6bbbb
Gupreet Singh <129745710+GurpreetSingh31@users.noreply.github.com>
2023-06-02 16:47:54 +0530

    Update is_prime.java
    Added the Scanner class and make input name object and taken the input from the user

--- Java/Maths/is_prime.java
+++ Java/Maths/is_prime.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class PrimeExample{    
  public static void main(String args[]){    
-  int i,m=0,flag=0;      
-  int n=3;//it is the number to be checked    
-  m=n/2;      
+  int i,m=0,flag=0;
+  Scanner input = new Scanner(System.in);// Creating the object of Scanner class
+  int n= input.nextInt(); //it is the number to be checked    
+   m = n /2;      
   if(n==0||n==1){  
    System.out.println(n+" is not prime number");      
   }else{  
