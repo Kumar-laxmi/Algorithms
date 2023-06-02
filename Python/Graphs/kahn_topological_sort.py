6291abbf701a53ba0c6b50273f9325fcc45e5fb4
Aishjahan <aishjahankhan7376@gmail.com>
2023-06-02 11:27:39 +0530

    removed codefactor issues

--- Python/Graphs/kahn_topological_sort.py
+++ Python/Graphs/kahn_topological_sort.py
@@ -69,12 +69,12 @@ class Graph:
 
 
 g = Graph(6)
-g.addEdge(5, 2);
-g.addEdge(5, 0);
-g.addEdge(4, 0);
-g.addEdge(4, 1);
-g.addEdge(2, 3);
-g.addEdge(3, 1);
+g.addEdge(5, 0)
+g.addEdge(5, 2)
+g.addEdge(4, 0)
+g.addEdge(4, 1)
+g.addEdge(2, 3)
+g.addEdge(3, 1)
 
 print ("Following is a Topological Sort of the given graph")
 g.topologicalSort()
