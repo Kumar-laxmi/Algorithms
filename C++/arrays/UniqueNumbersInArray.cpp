75116cd77a1ca5f518be01722ce2f422bda9bc36
HARSHSUHANE17 <84396068+HARSHSUHANE17@users.noreply.github.com>
2023-06-03 15:06:12 +0530

    Create UniqueNumbersInArray.cpp

---
+++ C++/arrays/UniqueNumbersInArray.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+    int findUnique(int a[], int n, int k) {
+       sort(a,a+n);
+       for(int i=0;i<n;i+=k){
+           if(a[i]!=a[i+1]){
+               return a[i];
+           }
+       }
+    }
+
+};
