666d83ade78f94dd66c5e842472933a1c180d800
Kom Senapati <Komnoob123@gmail.com>
2023-06-05 18:33:47 +0530

    Removed readme.md files under dp

--- C++/Dynamic-Programming/Fibonacci_Number.cpp
+++ C++/Dynamic-Programming/Fibonacci_Number.cpp
@@ -28,7 +28,7 @@ int fibonacci(int n)
     memo[0] = 0;
     memo[1] = 1;
 
-    for (int i = 0; i <= n; i++)
+    for (int i = 2; i <= n; i++)
     {
         memo[i] = memo[i - 1] + memo[i - 2];
     }
