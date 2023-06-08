bb5f394921b2a88207e80bf71768150454a3f050
shravanii2308 <113932534+shravanii2308@users.noreply.github.com>
2023-06-08 23:51:06 +0530

    Create  Fisher_Yates_Shuffle_Algorithm.cpp

---
+++ C++/arrays/Fisher_Yates_Shuffle_Algorithm.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <random> // Required for random number generation
+
+// Function to perform Fisher-Yates shuffle
+void fisherYatesShuffle(std::vector<int>& arr) {
+    int n = arr.size();
+    std::random_device rd; // Obtain a random seed from hardware
+    std::mt19937 rng(rd()); // Initialize a random number generator
+
+    for (int i = n - 1; i > 0; --i) {
+        std::uniform_int_distribution<int> dist(0, i); // Define a random distribution
+        int j = dist(rng); // Generate a random index between 0 and i (inclusive)
+        
+        // Swap the elements at indices i and j
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+int main() {
+    // Create an array of integers
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    // Print the original array
+    std::cout << "Original array: ";
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    // Shuffle the array using Fisher-Yates shuffle
+    fisherYatesShuffle(numbers);
+
+    // Print the shuffled array
+    std::cout << "Shuffled array: ";
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
