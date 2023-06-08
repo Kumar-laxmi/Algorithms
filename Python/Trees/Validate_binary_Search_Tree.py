82c340ff87524440c0385909e17d68f83267b5a4
mohitsingh1011 <mohitssu001@gmail.com>
2023-06-08 22:09:17 +0530

    Update Validate_binary_Search_Tree.py

--- Python/Trees/Validate_binary_Search_Tree.py
+++ Python/Trees/Validate_binary_Search_Tree.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
   root.left = Node(2)
   root.right = Node(5)
   # root.right.left = Node(7)
-  root.left.left = Node(1)
+  root.left.left = Node(1) 
   root.left.right = Node(3)
  
   # Function call
