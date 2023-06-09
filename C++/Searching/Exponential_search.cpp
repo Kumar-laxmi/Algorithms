6026a5de514f2e9028069a59ac2387c430f1be94
Kritika Singh <92994108+kritika2313@users.noreply.github.com>
2023-06-09 17:21:59 +0530

    Update Exponential_search.cpp

--- C++/Searching/Exponential_search.cpp
+++ C++/Searching/Exponential_search.cpp
@@ -63,9 +63,21 @@ int exponentialSearch(int arr[], int size, int target) {
 
 
 int main() {
-    int arr[] = {2, 4, 8, 16, 32, 64, 128, 256};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int target = 32;
+    int arr[100];
+    int size, target;
+
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + size);
+
+    cout << "Enter the target element: ";
+    cin >> target;
 
     int result = exponentialSearch(arr, size, target);
     if (result == -1) {
