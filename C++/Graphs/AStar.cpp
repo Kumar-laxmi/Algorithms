55bbc682861572ad5f0caea164888eb806619095
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2023-06-10 19:20:09 +0530

    AStar Algorithm in C++

--- C++/Graphs/AStar.cpp
+++ C++/Graphs/AStar.cpp
@@ -1,399 +1,89 @@
-// A C++ Program to implement A* Search Algorithm
-#include <bits/stdc++.h>
-using namespace std;
+/* Find Shortest Distance from source to destination using AStar Algorithm*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
 
-#define ROW 9
-#define COL 10
+const int INF = 1e9;
 
+// Structure to represent a node in the graph
+struct Node
+{
+    int id;        // Node ID
+    int distance;  // Shortest distance from the source
+    int heuristic; // Heuristic value
 
-typedef pair<int, int> Pair;
-typedef pair<double, pair<int, int> > pPair;
-struct cell {
-	int parent_i, parent_j;
-	// f = g + h
-	double f, g, h;
+    Node(int id, int distance, int heuristic) : id(id), distance(distance), heuristic(heuristic) {}
+
+    // Comparison operator for priority queue
+    bool operator<(const Node &other) const
+    {
+        return distance + heuristic > other.distance + other.heuristic;
+    }
 };
 
-bool isValid(int row, int col)
+// A* algorithm to find the shortest path
+void astar(const std::vector<std::vector<std::pair<int, int>>> &graph, int source, int destination, std::vector<int> &distances)
 {
+    int n = graph.size();
+    distances.resize(n, INF);
+    std::priority_queue<Node> pq;
+    pq.push(Node(source, 0, 0));
+    distances[source] = 0;
 
-	return (row >= 0) && (row < ROW) && (col >= 0)
-		&& (col < COL);
-}
+    while (!pq.empty())
+    {
+        Node current = pq.top();
+        pq.pop();
+        int u = current.id;
+        int dist = current.distance;
 
-bool isUnBlocked(int grid[][COL], int row, int col)
-{
+        if (u == destination)
+        {
+            break; // Reached the destination, terminate early
+        }
 
-	if (grid[row][col] == 1)
-		return (true);
-	else
-		return (false);
-}
+        if (dist > distances[u])
+        {
+            continue; // Ignore outdated entries in the priority queue
+        }
 
+        for (const auto &neighbor : graph[u])
+        {
+            int v = neighbor.first;
+            int weight = neighbor.second;
+            int newDist = dist + weight;
 
-bool isDestination(int row, int col, Pair dest)
-{
-	if (row == dest.first && col == dest.second)
-		return (true);
-	else
-		return (false);
-}
-
-double calculateHValue(int row, int col, Pair dest)
-{
-	// Return using the distance formula
-	return ((double)sqrt(
-		(row - dest.first) * (row - dest.first)
-		+ (col - dest.second) * (col - dest.second)));
-}
-
-void tracePath(cell cellDetails[][COL], Pair dest)
-{
-	printf("\nThe Path is ");
-	int row = dest.first;
-	int col = dest.second;
-
-	stack<Pair> Path;
-
-	while (!(cellDetails[row][col].parent_i == row
-			&& cellDetails[row][col].parent_j == col)) {
-		Path.push(make_pair(row, col));
-		int temp_row = cellDetails[row][col].parent_i;
-		int temp_col = cellDetails[row][col].parent_j;
-		row = temp_row;
-		col = temp_col;
-	}
-
-	Path.push(make_pair(row, col));
-	while (!Path.empty()) {
-		pair<int, int> p = Path.top();
-		Path.pop();
-		printf("-> (%d,%d) ", p.first, p.second);
-	}
-
-	return;
-}
-
-// A* Search Algorithm
-void aStarSearch(int grid[][COL], Pair src, Pair dest)
-{
-	// If the source is out of range
-	if (isValid(src.first, src.second) == false) {
-		printf("Source is invalid\n");
-		return;
-	}
-
-	// If the destination is out of range
-	if (isValid(dest.first, dest.second) == false) {
-		printf("Destination is invalid\n");
-		return;
-	}
-
-	// Either the source or the destination is blocked
-	if (isUnBlocked(grid, src.first, src.second) == false
-		|| isUnBlocked(grid, dest.first, dest.second)
-			== false) {
-		printf("Source or the destination is blocked\n");
-		return;
-	}
-
-	if (isDestination(src.first, src.second, dest)
-		== true) {
-		printf("We are already at the destination\n");
-		return;
-	}
-	bool closedList[ROW][COL];
-	memset(closedList, false, sizeof(closedList));
-	cell cellDetails[ROW][COL];
-
-	int i, j;
-
-	for (i = 0; i < ROW; i++) {
-		for (j = 0; j < COL; j++) {
-			cellDetails[i][j].f = FLT_MAX;
-			cellDetails[i][j].g = FLT_MAX;
-			cellDetails[i][j].h = FLT_MAX;
-			cellDetails[i][j].parent_i = -1;
-			cellDetails[i][j].parent_j = -1;
-		}
-	}
-
-	// Initialising the parameters of the starting node
-	i = src.first, j = src.second;
-	cellDetails[i][j].f = 0.0;
-	cellDetails[i][j].g = 0.0;
-	cellDetails[i][j].h = 0.0;
-	cellDetails[i][j].parent_i = i;
-	cellDetails[i][j].parent_j = j;
-
-
-	set<pPair> openList;
-	openList.insert(make_pair(0.0, make_pair(i, j)));
-	bool foundDest = false;
-
-	while (!openList.empty()) {
-		pPair p = *openList.begin();
-
-		openList.erase(openList.begin());
-
-		i = p.second.first;
-		j = p.second.second;
-		closedList[i][j] = true;
-		double gNew, hNew, fNew;
-
-		if (isValid(i - 1, j) == true) {
-			if (isDestination(i - 1, j, dest) == true) {
-				cellDetails[i - 1][j].parent_i = i;
-				cellDetails[i - 1][j].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-			else if (closedList[i - 1][j] == false
-					&& isUnBlocked(grid, i - 1, j)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.0;
-				hNew = calculateHValue(i - 1, j, dest);
-				fNew = gNew + hNew;
-				if (cellDetails[i - 1][j].f == FLT_MAX
-					|| cellDetails[i - 1][j].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i - 1, j)));
-
-					cellDetails[i - 1][j].f = fNew;
-					cellDetails[i - 1][j].g = gNew;
-					cellDetails[i - 1][j].h = hNew;
-					cellDetails[i - 1][j].parent_i = i;
-					cellDetails[i - 1][j].parent_j = j;
-				}
-			}
-		}
-
-		if (isValid(i + 1, j) == true) {
-			if (isDestination(i + 1, j, dest) == true) {
-				cellDetails[i + 1][j].parent_i = i;
-				cellDetails[i + 1][j].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-
-			else if (closedList[i + 1][j] == false
-					&& isUnBlocked(grid, i + 1, j)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.0;
-				hNew = calculateHValue(i + 1, j, dest);
-				fNew = gNew + hNew;
-
-				if (cellDetails[i + 1][j].f == FLT_MAX
-					|| cellDetails[i + 1][j].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i + 1, j)));
-					cellDetails[i + 1][j].f = fNew;
-					cellDetails[i + 1][j].g = gNew;
-					cellDetails[i + 1][j].h = hNew;
-					cellDetails[i + 1][j].parent_i = i;
-					cellDetails[i + 1][j].parent_j = j;
-				}
-			}
-		}
-
-		if (isValid(i, j + 1) == true) {
-			if (isDestination(i, j + 1, dest) == true) {
-				cellDetails[i][j + 1].parent_i = i;
-				cellDetails[i][j + 1].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-			else if (closedList[i][j + 1] == false
-					&& isUnBlocked(grid, i, j + 1)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.0;
-				hNew = calculateHValue(i, j + 1, dest);
-				fNew = gNew + hNew;
-
-				if (cellDetails[i][j + 1].f == FLT_MAX
-					|| cellDetails[i][j + 1].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i, j + 1)));
-
-					cellDetails[i][j + 1].f = fNew;
-					cellDetails[i][j + 1].g = gNew;
-					cellDetails[i][j + 1].h = hNew;
-					cellDetails[i][j + 1].parent_i = i;
-					cellDetails[i][j + 1].parent_j = j;
-				}
-			}
-		}
-		if (isValid(i, j - 1) == true) {
-			if (isDestination(i, j - 1, dest) == true) {
-				cellDetails[i][j - 1].parent_i = i;
-				cellDetails[i][j - 1].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-			else if (closedList[i][j - 1] == false
-					&& isUnBlocked(grid, i, j - 1)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.0;
-				hNew = calculateHValue(i, j - 1, dest);
-				fNew = gNew + hNew;
-				if (cellDetails[i][j - 1].f == FLT_MAX
-					|| cellDetails[i][j - 1].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i, j - 1)));
-					cellDetails[i][j - 1].f = fNew;
-					cellDetails[i][j - 1].g = gNew;
-					cellDetails[i][j - 1].h = hNew;
-					cellDetails[i][j - 1].parent_i = i;
-					cellDetails[i][j - 1].parent_j = j;
-				}
-			}
-		}
-
-		if (isValid(i - 1, j + 1) == true) {
-			if (isDestination(i - 1, j + 1, dest) == true) {
-				cellDetails[i - 1][j + 1].parent_i = i;
-				cellDetails[i - 1][j + 1].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-			else if (closedList[i - 1][j + 1] == false
-					&& isUnBlocked(grid, i - 1, j + 1)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.414;
-				hNew = calculateHValue(i - 1, j + 1, dest);
-				fNew = gNew + hNew;
-				if (cellDetails[i - 1][j + 1].f == FLT_MAX
-					|| cellDetails[i - 1][j + 1].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i - 1, j + 1)));
-
-					cellDetails[i - 1][j + 1].f = fNew;
-					cellDetails[i - 1][j + 1].g = gNew;
-					cellDetails[i - 1][j + 1].h = hNew;
-					cellDetails[i - 1][j + 1].parent_i = i;
-					cellDetails[i - 1][j + 1].parent_j = j;
-				}
-			}
-		}
-
-		if (isValid(i - 1, j - 1) == true) {
-			if (isDestination(i - 1, j - 1, dest) == true) {
-				cellDetails[i - 1][j - 1].parent_i = i;
-				cellDetails[i - 1][j - 1].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-			else if (closedList[i - 1][j - 1] == false
-					&& isUnBlocked(grid, i - 1, j - 1)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.414;
-				hNew = calculateHValue(i - 1, j - 1, dest);
-				fNew = gNew + hNew;
-				if (cellDetails[i - 1][j - 1].f == FLT_MAX
-					|| cellDetails[i - 1][j - 1].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i - 1, j - 1)));
-					cellDetails[i - 1][j - 1].f = fNew;
-					cellDetails[i - 1][j - 1].g = gNew;
-					cellDetails[i - 1][j - 1].h = hNew;
-					cellDetails[i - 1][j - 1].parent_i = i;
-					cellDetails[i - 1][j - 1].parent_j = j;
-				}
-			}
-		}
-		if (isValid(i + 1, j + 1) == true) {
-			if (isDestination(i + 1, j + 1, dest) == true) {
-				cellDetails[i + 1][j + 1].parent_i = i;
-				cellDetails[i + 1][j + 1].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-			else if (closedList[i + 1][j + 1] == false
-					&& isUnBlocked(grid, i + 1, j + 1)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.414;
-				hNew = calculateHValue(i + 1, j + 1, dest);
-				fNew = gNew + hNew;
-				if (cellDetails[i + 1][j + 1].f == FLT_MAX
-					|| cellDetails[i + 1][j + 1].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i + 1, j + 1)));
-
-					cellDetails[i + 1][j + 1].f = fNew;
-					cellDetails[i + 1][j + 1].g = gNew;
-					cellDetails[i + 1][j + 1].h = hNew;
-					cellDetails[i + 1][j + 1].parent_i = i;
-					cellDetails[i + 1][j + 1].parent_j = j;
-				}
-			}
-		}
-
-		if (isValid(i + 1, j - 1) == true) {
-			if (isDestination(i + 1, j - 1, dest) == true) {
-				cellDetails[i + 1][j - 1].parent_i = i;
-				cellDetails[i + 1][j - 1].parent_j = j;
-				printf("The destination cell is found\n");
-				tracePath(cellDetails, dest);
-				foundDest = true;
-				return;
-			}
-
-			else if (closedList[i + 1][j - 1] == false
-					&& isUnBlocked(grid, i + 1, j - 1)
-							== true) {
-				gNew = cellDetails[i][j].g + 1.414;
-				hNew = calculateHValue(i + 1, j - 1, dest);
-				fNew = gNew + hNew;
-				if (cellDetails[i + 1][j - 1].f == FLT_MAX
-					|| cellDetails[i + 1][j - 1].f > fNew) {
-					openList.insert(make_pair(
-						fNew, make_pair(i + 1, j - 1)));
-					cellDetails[i + 1][j - 1].f = fNew;
-					cellDetails[i + 1][j - 1].g = gNew;
-					cellDetails[i + 1][j - 1].h = hNew;
-					cellDetails[i + 1][j - 1].parent_i = i;
-					cellDetails[i + 1][j - 1].parent_j = j;
-				}
-			}
-		}
-	}
-	if (foundDest == false)
-		printf("Failed to find the Destination Cell\n");
-
-	return;
+            if (newDist < distances[v])
+            {
+                int heuristic = 0; // Calculate the heuristic value for each node here
+                distances[v] = newDist;
+                pq.push(Node(v, newDist, heuristic));
+            }
+        }
+    }
 }
 
 int main()
 {
-	int grid[ROW][COL]
-		= { { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
-			{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
-			{ 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
-			{ 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
-			{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
-			{ 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
-			{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
-			{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
-			{ 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 } };
+    int n = 6; // Number of nodes in the graph
+    std::vector<std::vector<std::pair<int, int>>> graph(n);
 
-	Pair src = make_pair(8, 0);
-	Pair dest = make_pair(0, 0);
+    // Add edges to the graph
+    graph[0].emplace_back(1, 2);
+    graph[0].emplace_back(2, 4);
+    graph[1].emplace_back(2, 1);
+    graph[1].emplace_back(3, 7);
+    graph[2].emplace_back(4, 3);
+    graph[3].emplace_back(4, 1);
+    graph[3].emplace_back(5, 5);
+    graph[4].emplace_back(5, 2);
 
-	aStarSearch(grid, src, dest);
+    std::vector<int> distances;
+    astar(graph, 0, 5, distances);
 
-	return (0);
+    // Print shortest distance to the destination
+    std::cout << "Shortest distance from node 0 to node 5: " << distances[5] << std::endl;
+
+    return 0;
 }
