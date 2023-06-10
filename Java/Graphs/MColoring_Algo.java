2df234f93937190a6bb8af577bfdc752aa569345
dhruti26 <106862356+dhruti26@users.noreply.github.com>
2023-06-10 12:01:43 +0530

    MColoring Algorithm commited

---
+++ Java/Graphs/MColoring_Algo.java
@@ -0,0 +1,85 @@
+ // M-Coloring Algorithm in java
+package Java.Graphs;
+import java.util.*;
+
+public class MColoring_Algo{
+     final int V = 4;
+     int color[];
+
+      boolean isSafe(int v, int graph[][], int color[], int c)
+    {
+        for (int i = 0; i < V; i++){
+            if (graph[v][i] == 1 && c == color[i]){
+                //if adjacent vertices has same color return false
+                return false;
+            }
+        }
+        return true;
+    }  
+
+     boolean giveColor(int graph[][], int m,int color[], int v)
+    {
+        if (v == V){        
+            // for all vertices, color is assigned return true       
+            return true;
+        }
+ 
+        for (int c = 1; c <= m; c++) {
+            if (isSafe(v, graph, color, c)) {
+                color[v] = c;
+ 
+                if (giveColor(graph, m, color,v + 1))
+                    return true;
+ 
+                color[v] = 0;
+            }
+        }
+        return false;
+    }
+
+     boolean MColoring(int graph[][], int m)
+    {
+        color = new int[V];
+        for (int i = 0; i < V; i++)
+            color[i] = 0;
+ 
+        // Start with vertex 0 and give color
+        if (!giveColor(graph, m, color, 0)) {
+            System.out.println("Solution does not exist");
+            return false;
+        }
+ 
+        printSolution(color);
+        return true;
+    }
+ 
+
+    void printSolution(int color[])
+    {
+        System.out.println("Solution Exists:" + " The colors assigned to vertices are: ");
+        for (int i = 0; i < V; i++){
+            System.out.print(" " + color[i] + " ");
+        }
+        System.out.println();
+    }
+   public static void main(String args[]) {
+       MColoring_Algo color = new MColoring_Algo();
+       /* Given Graph  and  m = 3
+      (3)---(2)
+       |   / |
+       |  /  |
+       | /   |
+      (0)---(1)
+    */
+   
+    int graph[][] = {
+        { 0, 1, 1, 1 },
+        { 1, 0, 1, 0 },
+        { 1, 1, 0, 1 },
+        { 1, 0, 1, 0 },
+    };
+  
+    int m = 3;
+    color.MColoring(graph,m);
+   }
+}
---
+++ Python/Graphs/MColoring_Algorithm.py
@@ -0,0 +1,58 @@
+# MColoring Algorithm in python
+def isSafe(graph, color):
+ 
+    for i in range(4):
+        for j in range(i + 1, 4):
+            if (graph[i][j] and color[j] == color[i]):
+                return False
+    return True
+
+def MColoring(graph, m, i, color):
+ 
+    # if current index reached end
+    if (i == 4):   
+ 
+        
+        if (isSafe(graph, color)):
+ 
+      
+            printSolution(color)
+            return True
+        return False
+ 
+    # Give color from 1 to m
+    for j in range(1, m + 1):
+        color[i] = j
+ 
+       
+        if (MColoring(graph, m, i + 1, color)):
+            return True
+        color[i] = 0   #given color does not reach to solution ,remove color
+    return False
+
+def printSolution(color):
+    print("Solution Exists:" " The colors assigned to vertices are:   ")
+    for i in range(4):
+        print(color[i], end=" ")
+
+if __name__ == '__main__':
+    # /* Given Graph  and  m = 3
+    #  (3)---(2)
+    #  |   / |
+    #  |  /  |
+    #  | /   |
+    # (0)---(1)
+    # */
+      graph = [
+        [0, 1, 1, 1],
+        [1, 0, 1, 0],
+        [1, 1, 0, 1],
+        [1, 0, 1, 0],
+      ]
+      m = 3  # Number of colors
+
+      color = [0 for i in range(4)]   #color initailized with 0
+    
+      if (not MColoring(graph, m, 0, color)):       
+         print("Solution does not exist")
+    
