b1b76378c45535b884eb58e12834eee4e0c11f9d
Praveen Kumar <singhsatyam312005@gmail.com>
2023-06-11 18:55:53 +0530

    Update Hill_climbing.py

--- Python/Searching/Hill_climbing.py
+++ Python/Searching/Hill_climbing.py
@@ -12,6 +12,9 @@ The code uses the following functions:
 * `Hill_climbing()`: This function implements the Hill Climbing algorithm.
 * `main()`: This function sets the seed for random number generation and then runs the Hill Climbing algorithm.
 """
+import math
+import random
+import time
 
 # Define the search space boundaries.
 LOWER_LIMIT = -10
@@ -25,45 +28,45 @@ MAX_ITERATIONS = 100
 
 # This function returns the value of the objective function at the given point `x`.
 def purpose_function(x):
-  # Calculate the objective value of x.
-  return -math.pow(x, 2) + 5
+    # Calculate the objective value of x.
+    return -math.pow(x, 2) + 5
 
 # This function returns a random number between the given lower and upper bounds.
 def random_number(lower, upper):
-  # Generate a random number between the given lower and upper bounds.
-  return ((float)random() / RAND_MAX) * (upper - lower) + lower
+    # Generate a random number between the given lower and upper bounds.
+    return random.random() * (upper - lower) + lower
 
 # This function implements the Hill Climbing algorithm for finding the maximum of a function.
-def Hill_climbing(lower_bound, upper_bound, step, max_iter):
-  # Initialize the current best solution.
-  current_answer = random_number(lower_bound, upper_bound)
-  current_val = purpose_function(current_answer)
+def hill_climbing(lower_bound, upper_bound, step, max_iter):
+    # Initialize the current best solution.
+    current_answer = random_number(lower_bound, upper_bound)
+    current_val = purpose_function(current_answer)
 
-  # Perform the iterations.
-  for i in range(max_iter):
-    # Generate a random neighbor.
-    neighbor = current_answer + random_number(-step, step)
-    neighbor = max(min(neighbor, upper_bound), lower_bound)  # Ensure the neighbor is within the boundaries
+    # Perform the iterations.
+    for i in range(max_iter):
+        # Generate a random neighbor.
+        neighbor = current_answer + random_number(-step, step)
+        neighbor = max(min(neighbor, upper_bound), lower_bound)  # Ensure the neighbor is within the boundaries
 
-    # Calculate the objective value of the neighbor.
-    neighbor_val = purpose_function(neighbor)
+        # Calculate the objective value of the neighbor.
+        neighbor_val = purpose_function(neighbor)
 
-    # Update the current answer if the neighbor is better.
-    if neighbor_val > current_val:
-      current_answer = neighbor
-      current_val = neighbor_val
+        # Update the current answer if the neighbor is better.
+        if neighbor_val > current_val:
+            current_answer = neighbor
+            current_val = neighbor_val
 
-  # Print the best solution found.
-  print("Best solution: " + str(current_answer))
-  print("Best value: " + str(current_val))
+    # Print the best solution found.
+    print("Best solution: " + str(current_answer))
+    print("Best value: " + str(current_val))
 
 # This function sets the seed for random number generation.
 def main():
-  # Set the seed for random number generation.
-  random.seed(time())
+    # Set the seed for random number generation.
+    random.seed(time.time())
 
-  # Run the Hill Climbing algorithm.
-  Hill_climbing(LOWER_LIMIT, UPPER_LIMIT, STEP_SIZE, MAX_ITERATIONS)
+    # Run the Hill Climbing algorithm.
+    hill_climbing(LOWER_LIMIT, UPPER_LIMIT, STEP_SIZE, MAX_ITERATIONS)
 
 if __name__ == "__main__":
-  main()
+    main()
