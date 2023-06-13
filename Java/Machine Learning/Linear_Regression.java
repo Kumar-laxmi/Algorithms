4328a1f38a61544bd4e71e7b7acd91d10abca464
Praveen Kumar <singhsatyam312005@gmail.com>
2023-06-13 15:57:47 +0530

    Update Linear_Regression.java

--- Java/Machine Learning/Linear_Regression.java
+++ Java/Machine Learning/Linear_Regression.java
@@ -11,7 +11,7 @@ public class Linear_Regression {
         slopes = new double[x[0].length]; // Initialize the slopes array with the length of variables in x
         double[] numerators = new double[x[0].length]; // Array to store the numerators of the slopes calculation
         double[] denominators = new double[x[0].length]; // Array to store the denominators of the slopes calculation
-        
+
         // Iterate over each variable in x
         for (int i = 0; i < x[0].length; i++) {
             // Iterate over each data point in x and y
@@ -21,7 +21,7 @@ public class Linear_Regression {
             }
             slopes[i] = numerators[i] / denominators[i]; // Calculate the slope (beta1) for the current variable
         }
-        
+
         intercept = yMean - dotProduct(xMeans, slopes); // Calculate the intercept (beta0) of the regression line
     }
 
@@ -42,11 +42,11 @@ public class Linear_Regression {
     private double[] calculateMeans(double[][] values) {
         // Calculate the means of each variable in a 2D array of values
         double[] means = new double[values[0].length]; // Array to store the means
-        
+
         // Iterate over each variable in the 2D array
         for (int i = 0; i < values[0].length; i++) {
             double sum = 0.0;
-            
+
             // Iterate over each data point for the current variable
             for (double[] row : values) {
                 sum += row[i]; // Add the value to the sum
@@ -59,7 +59,7 @@ public class Linear_Regression {
     private double dotProduct(double[] a, double[] b) {
         // Calculate the dot product of two arrays
         double result = 0.0;
-        
+
         // Iterate over the elements of the arrays and calculate the dot product
         for (int i = 0; i < a.length; i++) {
             result += a[i] * b[i]; // Multiply the corresponding elements and add to the result
@@ -73,14 +73,33 @@ public class Linear_Regression {
             {1.0, 2.0},
             {2.0, 3.0},
             {3.0, 4.0},
-            {4.0, 5.0}
+            {4.0, 5.0},
+            {5.0, 6.0},
+            {6.0, 7.0},
+            {7.0, 8.0},
+            {8.0, 9.0},
+            {9.0, 10.0},
+            {10.0, 11.0},
+            {11.0, 12.0},
+            {12.0, 13.0},
+            {13.0, 14.0},
+            {14.0, 15.0},
+            {15.0, 16.0},
+            {16.0, 17.0},
+            {17.0, 18.0},
+            {18.0, 19.0},
+            {19.0, 20.0},
+            {20.0, 21.0}
         };
-        double[] y = {3.0, 5.0, 7.0, 9.0};
+        double[] y = {
+            3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0,
+            25.0, 27.0, 29.0, 31.0, 33.0, 35.0, 37.0, 39.0, 41.0
+        };
 
         Linear_Regression regression = new Linear_Regression(); // Create an instance of Linear_Regression
         regression.train(x, y); // Train the regression model using the input data
 
-        double[] newX = {5.0, 6.0};
+        double[] newX = {21.0, 22.0};
         double predictedY = regression.predict(newX); // Make a prediction for the new input values
         System.out.println("Predicted Y for newX: " + predictedY); // Print the predicted value of y
     }
