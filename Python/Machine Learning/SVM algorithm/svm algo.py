a57427952c5d5def7ab2720f3a3617087099a23a
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-06-13 18:38:21 +0530

    commit

--- Python/Machine Learning/SVM algorithm/svm algo.py
+++ Python/Machine Learning/SVM algorithm/svm algo.py
@@ -46,3 +46,29 @@ svm.fit(X_train, y_train)
 # Make predictions on the testing set
 predictions = svm.predict(X_test)
 print(predictions)
+
+
+# #Support Vector Machine (SVM) Algorithm - Python 
+
+# ##Features
+
+# Utilizes the NumPy library for efficient numerical operations.
+# Simple and concise code using object-oriented programming.
+# Implements a basic version of the SVM algorithm with a linear kernel.
+# Supports a limited dataset size due to performance considerations.
+# Demonstrates the usage of a Python class to encapsulate SVM functionalities.
+
+# ##Dependencies
+
+# Python 3.x
+# NumPy
+
+
+# ##How to Run
+
+# Ensure that Python and NumPy are installed on your system.
+# Open the svm algo.py file.
+# Modify the dataset generation parameters (e.g., sample size, feature size) as desired.
+# Run the script.
+# The program will train the SVM model on a randomly generated dataset and make predictions on a test set.
+# The predicted labels will be displayed in the console.
