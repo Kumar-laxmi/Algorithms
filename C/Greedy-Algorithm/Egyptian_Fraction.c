f394e326138d689e38ccd9e45e85d8abbab15edb
Shubhi Singh <shubhisingh1414.12@gmail.com>
2023-06-14 20:04:37 +0530

    Final commit

--- C/Greedy-Algorithm/Egyptian_Fraction.c
+++ C/Greedy-Algorithm/Egyptian_Fraction.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-void egyptianFraction(int n, int d)
+void egyptian_Fraction(int n, int d)
 {
     // When Both Numerator and denominator becomes zero then we simply return;
     if (d == 0 || n == 0)
@@ -20,22 +20,22 @@ void egyptianFraction(int n, int d)
     if (n > d)
     {
         printf("%d + ", n / d);
-        egyptianFraction(n % d, d);
+        egyptian_Fraction(n % d, d);
         return;
     }
     int x = d / n + 1;
     printf("1/%d + ", x);
-    egyptianFraction(n * x - d, d * x);
+    egyptian_Fraction(n * x - d, d * x);
 }
 
 int main()
 {
-    int numerator, denominator;
+    int num, den;
     printf("Enter Numerator: ");
-    scanf("%d", &numerator);
+    scanf("%d", &num);
     printf("Enter Denominator:");
-    scanf("%d", &denominator);
-    printf("Egyptian Fraction representation of %d/%d is\n", numerator, denominator);
-    egyptianFraction(numerator, denominator);
+    scanf("%d", &den);
+    printf("Egyptian Fraction representation of %d/%d is\n", num, den);
+    egyptian_Fraction(num, den);
     return 0;
 }
