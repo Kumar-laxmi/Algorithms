a17fecba97e093989b3889d986ac6e3e61029678
Shubhi Singh <shubhisingh1414.12@gmail.com>
2023-06-14 13:37:09 +0530

    Added Egyptian Fraction

---
+++ C/Greedy-Algorithm/Egyptian_Fraction.c
@@ -0,0 +1,37 @@
+/* C program to print a fraction in Egyptian Form using Greedy Algorithm*/
+
+#include <stdio.h>
+
+void egyptianFraction(int n, int d)
+{
+    // When Both Numerator and denominator becomes zero then we simply return;
+    if (d == 0 || n == 0)
+        return;
+    if (d % n == 0)
+    {
+        printf("1/%d", d / n);
+        return;
+    }
+    if (n % d == 0)
+    {
+        printf("%d", n / d);
+        return;
+    }
+    if (n > d)
+    {
+        printf("%d + ", n / d);
+        egyptianFraction(n % d, d);
+        return;
+    }
+    int x = d / n + 1;
+    printf("1/%d + ", x);
+    egyptianFraction(n * x - d, d * x);
+}
+
+int main()
+{
+    int numerator = 6, denominator = 14;
+    printf("Egyptian Fraction representation of %d/%d is\n", numerator, denominator);
+    egyptianFraction(numerator, denominator);
+    return 0;
+}
---
+++ Python/Greedy-Algorithm/Egyptian_Fraction.py
@@ -0,0 +1,38 @@
+# import math package to use ceiling function
+import math
+
+# define a function egyptianFraction which receive parameter nr as numerator and dr as denominator
+def egyptianFraction(nr, dr):
+
+	print("The Egyptian Fraction " +
+		"Representation of {0}/{1} is".
+				format(nr, dr), end="\n")
+
+	# empty list ef to store denominator
+	ef = []
+
+	# while loop runs until fraction becomes 0 i.e, numerator becomes 0
+	while nr != 0:
+
+		# taking ceiling
+		x = math.ceil(dr / nr)
+
+		# storing value in ef list
+		ef.append(x)
+
+		# updating new nr and dr
+		nr = x * nr - dr
+		dr = dr * x
+
+	# printing the values
+	for i in range(len(ef)):
+		if i != len(ef) - 1:
+			print(" 1/{0} +" .
+					format(ef[i]), end = " ")
+		else:
+			print(" 1/{0}" .
+					format(ef[i]), end = " ")
+
+# calling the function
+egyptianFraction(6, 14)
+
