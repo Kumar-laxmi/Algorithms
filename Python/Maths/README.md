0c2a8ecb3bb1e2b168e73245b3b01c4032a57cd8
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-06-14 16:58:52 +0530

    Bi section code updated

--- Python/Maths/README.md
+++ Python/Maths/README.md
@@ -1,1 +1,15 @@
 # Algorithms to compute mathematical functions
+Algroithms related to the mathematical functions are presented here.
+
+## Root Finding Methods
+### Contents
+- Bi Section method
+
+
+### Details
+#### Bi Section
+In this method we will assume that the answer is in between the curve inside the intervals.
+
+If curve really touches x-axis inside the interval then on every iteration we will get closer and closer to the root i.e. the point where the equation touches x-axis
+
+This method has a convergence of 1 (i.e. linear convergence)
--- Python/Maths/bisection.py
+++ Python/Maths/bisection.py
@@ -1,43 +1,73 @@
-# Python program for implementation
-# of Bisection Method for
-# solving equations
+# Bi Section Algorithm
 
+# Working
+"""
+    provide the equation and then an interval with an iteration limit.
 
-# An example function whose
-# solution is determined using
-# Bisection Method.
-# The function is x^3 - x^2 + 2
-def func(x):
-	return x*x*x - x*x + 2
+    The result will be either the root answer if found or not found.
+"""
 
-# Prints root of func(x)
-# with error of EPSILON
-def bisection(a,b):
+# Limitations
+"""
+    This is designed only for 1 variable
+"""
 
-	if (func(a) * func(b) >= 0):
-		print("You have not assumed right a and b\n")
-		return
+# Creating a class for function objects
+class f():
+    def __init__(self,eq: str):
+        self.eq = eq
+    def setEq(self,eq: str):
+        self.eq = eq 
+    def getEq(self):
+        return self.eq
+    def getVal(self,val: float):
+        equate = self.eq.replace("x",str(val))
+        res = eval(equate)
+        return res
 
-	c = a
-	while ((b-a) >= 0.01):
+def biSection(func: f,low_limit: float,up_limit: float,iter: int):
+    # The response container
+    result = {
+        "status":"not found",
+        "answer":0,
+        "remarks":None,
+    }
 
-		# Find middle point
-		c = (a+b)/2
+    # Check for possibility for root
+    if (func.getVal(low_limit)*func.getVal(up_limit) > 0):
+        result["remarks"] = "roots doesnt exist"
+        return result
 
-		# Check if middle point is root
-		if (func(c) == 0.0):
-			break
+    # Main Bi section code
+    for i in range(iter):
+        x = float(low_limit + up_limit)/2
+        res = func.getVal(x)
 
-		# Decide the side to repeat the steps
-		if (func(c)*func(a) < 0):
-			b = c
-		else:
-			a = c
-			
-	print("The value of root is : ","%.4f"%c)
-	
-# Driver code
-# Initial values assumed
-a =-200
-b = 300
-bisection(a, b)
+        if (res > 0):
+            up_limit = x
+        elif (res < 0):
+            low_limit = x
+        else:
+            result["status"] = "found"
+            break
+    result["answer"] = x
+    return result
+
+##### Main Function ######
+eq = input("Enter the equation:")
+func = f(eq)
+
+low_limit = float(input("Enter the lower limit:"))
+upper_limit = float(input("Enter the upper limit:"))
+
+iter = int(input("Enter the number of iterations:"))
+
+res = biSection(func,low_limit,upper_limit,iter)
+
+if (res["remarks"]):
+    print(res["remarks"])
+else:
+    if (res["status"] == "found"):
+        print(f"The Root is x={res['answer']}")
+    else:
+        print(f"The Nearest Root is x={res['answer']}")
