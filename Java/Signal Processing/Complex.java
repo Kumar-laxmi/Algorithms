b859b8ff8dd2bfc1f5eaea877cfce4a989e0300d
Aryan Sharma <128261659+Aryan-d13@users.noreply.github.com>
2023-06-18 19:03:49 +0530

    Create Complex.java

---
+++ Java/Signal Processing/Complex.java
@@ -0,0 +1,32 @@
+class Complex {
+    private final double real;
+    private final double imaginary;
+
+    public Complex(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public Complex add(Complex other) {
+        double realSum = this.real + other.real;
+        double imaginarySum = this.imaginary + other.imaginary;
+        return new Complex(realSum, imaginarySum);
+    }
+
+    public Complex subtract(Complex other) {
+        double realDiff = this.real - other.real;
+        double imaginaryDiff = this.imaginary - other.imaginary;
+        return new Complex(realDiff, imaginaryDiff);
+    }
+
+    public Complex multiply(Complex other) {
+        double realProduct = this.real * other.real - this.imaginary * other.imaginary;
+        double imaginaryProduct = this.real * other.imaginary + this.imaginary * other.real;
+        return new Complex(realProduct, imaginaryProduct);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + real + ", " + imaginary + ")";
+    }
+}
