69d48cb1790c2a132b7e30285e29b36034b803ec
Simran <simran@git.com>
2023-06-21 00:03:22 +0530

    solved code factor issues

--- C++/Concurrent Algorithms/semaphore.cpp
+++ C++/Concurrent Algorithms/semaphore.cpp
@@ -1,12 +1,10 @@
 struct semaphore {
-    
 	enum value(0, 1);
 
 	// q contains all Process Control Blocks (PCBs)
 	// corresponding to processes got blocked
 	// while performing down operation.
 	Queue<process> q;
-
 };
 P(semaphore s)
 {
@@ -24,7 +22,6 @@ V(Semaphore s)
 		s.value = 1;
 	}
 	else {
-
 		// select a process from waiting queue
 		Process p = q.front();
 		// remove the process from waiting as it has been
