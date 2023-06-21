862a2fc31d1ad9250cc8840945696aa636ccd6c6
VIVEK SATI <viveksati2002@gmail.com>
2023-06-21 19:18:43 +0530

    added Kruskal.cpp

---
+++ C++/Greedy-Algorithm/Kruskal.cpp
@@ -0,0 +1,78 @@
+// Cpp-program to implement famous Kruskal's Algorithm
+// Kruskal's Algorithm is a greedy algorithm that is used to find the minimum spanning tree from a graph.
+
+// The idea is to sort all edges of given graph in increasing order, and then keep on adding new edges and nodes in the spanning tree if the added edge does not form a cycle.
+
+
+/* A minimum spanning tree is constructed from a weighted & undirected graph, so it:
+-> Connects all nodes / vertices
+-> Has no cycles
+-> Has smallest possible sum of edge weights.*/
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int maximum = 100000;
+int no[maximum], nodes, edges;
+pair <long long, pair<int, int>> k[maximum];
+
+int base(int a)
+{
+    while (no[a] != a)
+    {
+        no[a] = no[no[a]];
+        a = no[a];
+    }
+    return a;
+}
+
+void temp(int a, int b)
+{
+    int k = base(a);
+    int l = base(b);
+    no[k] = no[l];
+}
+
+long long krus(pair<long long, pair<int, int>> k[])
+{
+    int a, b;
+    long long cost, minCost = 0;
+    for (int i = 0; i < edges; ++i)
+    {
+        a = k[i].second.first;
+        b = k[i].second.second;
+        cost = k[i].first;
+        if (base(a) != base(b))
+        {
+            minCost += cost;
+            temp(a, b);
+        }
+    }
+    return minCost;
+}
+
+int main()
+{
+    int a, b;
+    long long w, cost, minCost;
+
+    for (int i = 0; i < maximum; ++i)
+    {
+        no[i] = i;
+    }
+
+    cout << "Enter Nodes and edges";
+    cin >> nodes >> edges;
+    for (int i = 0; i < edges; ++i)
+    {
+        cout << "Enter the value of a, b and edges ";
+        cin >> a >> b >> w;
+        k[i] = make_pair(w, make_pair(a, b));
+    }
+    sort(k, k + edges);
+    minCost = krus(k);
+    cout << "Minimum cost is " << minCost << endl;
+    return 0;
+}
+
+// Time Complexity: O(E log N)
---
+++ C/Greedy-Algorithm/Kruskal.c
@@ -0,0 +1,19 @@
+// C-program to implement famous Kruskal's Algorithm
+// Kruskal's Algorithm is a greedy algorithm that is used to find the minimum spanning tree from a graph.
+
+// The idea is to sort all edges of given graph in increasing order, and then keep on adding new edges and nodes in the spanning tree if the added edge does not form a cycle.
+
+
+/* A minimum spanning tree is constructed from a weighted & undirected graph, so it:
+-> Connects all nodes / vertices
+-> Has no cycles
+-> Has smallest possible sum of edge weights.*/
+#include<stdio.h>
+
+int main()
+{
+
+}
+
+
+// Time Complexity: O(E log N)
