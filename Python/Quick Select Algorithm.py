93016432a44099db8fa9fb803bd3bc158937a653
harshitaaa8 <121786696+harshitaaa8@users.noreply.github.com>
2023-06-23 15:15:30 +0530

    Update Quick Select Algorithm.py

--- Python/Quick Select Algorithm.py
+++ Python/Quick Select Algorithm.py
@@ -2,37 +2,37 @@
 
 # Partition i.e considering the last element as pivot and move all smaller element to left of it and greater elements to right
 
-def partition(array, l, r):
+def partition(array, left, right):
 	
-	x = array[r]
-	i = l
-	for j in range(l, r):
+	x = array[right]
+	i = left
+	for j in range(left, right):
 		if array[j] <= x:
 			array[i], array[j] = array[j], array[i]
 			i += 1
-	array[i], array[r] = array[r], array[i]
+	array[i], array[right] = array[right], array[i]
 	return i
 
 # Finds the kth position (of the sorted array)
 # Assuming: all elements in arr[] are distinct
-def kthsmallest(arr, l, r, k):
+def kthsmallest(arr, left, right, k):
 
 	# If k is smaller than number of elements in array
-	if (k > 0 and k <= r - l + 1):
+	if (k > 0 and k <= right - left + 1):
 
 		# Partition the array around last element and get position of pivot element
-		index = partition(arr, l, r)
+		index = partition(arr, left, right)
 
 		# If position is same as k
-		if (index - l == k - 1):
+		if (index - left == k - 1):
 			return arr[index]
 
 		# If position is more, recur for left subarray
-		if (index - l > k - 1):
-			return kthsmallest(arr, l, index - 1, k)
+		if (index - left > k - 1):
+			return kthsmallest(arr, left, index - 1, k)
 
 		# Else recur for right subarray
-		return kthsmallest(arr, index + 1, r, k - index + l - 1)
+		return kthsmallest(arr, index + 1, right, k - index + left - 1)
 	print("index out of bound")
 
 	
