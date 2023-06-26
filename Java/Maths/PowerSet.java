84b83f46aa5fed8db3714937b465751aac3f6542
VISHALGUPTA100803 <96491059+VISHALGUPTA100803@users.noreply.github.com>
2023-06-26 21:08:32 +0530

    Power set of a set of elements in C, C++, Java and Python #1339 #1588

--- Java/Maths/PowerSet.java
+++ Java/Maths/PowerSet.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Main {
+public class PowerSet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
