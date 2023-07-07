505ab19e8e9d78f5d8f110529fddd68438ec02bb
rayarka26 <74251803+rayarka26@users.noreply.github.com>
2023-07-07 23:17:21 +0530

    Update Ant_Bee_Colony_Optimization.py

--- Python/Optimization/Ant_Bee_Colony_Optimization.py
+++ Python/Optimization/Ant_Bee_Colony_Optimization.py
@@ -11,57 +11,57 @@ lower_bound = -5.12
 upper_bound = 5.12
 
 # Define the ABC algorithm
-def artificial_bee_colony(n_iterations, n_employed_bees, n_onlooker_bees, n_trials):
+def artificial_bee_colony(n_iterations, n_employed_individuals, n_onlooker_individuals, n_trials):
     # Initialize the population
     population = []
-    for _ in range(n_employed_bees):
-        solution = np.random.uniform(lower_bound, upper_bound)
-        population.append(solution)
+    for _ in range(n_employed_individuals):
+        individual = np.random.uniform(lower_bound, upper_bound)
+        population.append(individual)
 
     # Main loop
     for iteration in range(n_iterations):
         # Employed bees phase
-        for i in range(n_employed_bees):
-            solution = population[i]
-            new_solution = generate_neighbor_solution(solution, population)
-            if objective_function(new_solution) < objective_function(solution):
-                population[i] = new_solution
+        for i in range(n_employed_individuals):
+            individual = population[i]
+            new_individual = generate_neighbor_individual(individual, population)
+            if objective_function(new_individual) < objective_function(individual):
+                population[i] = new_individual
 
         # Calculate the fitness values
-        fitness = [1 / (objective_function(solution) + 0.01) for solution in population]
+        fitness = [1 / (objective_function(individual) + 0.01) for individual in population]
         total_fitness = sum(fitness)
 
         # Onlooker bees phase
-        for i in range(n_onlooker_bees):
-            # Select a solution based on its fitness value
+        for i in range(n_onlooker_individuals):
+            # Select an individual based on its fitness value
             probabilities = [f / total_fitness for f in fitness]
-            selected_index = np.random.choice(range(n_employed_bees), p=probabilities)
-            selected_solution = population[selected_index]
+            selected_index = np.random.choice(range(n_employed_individuals), p=probabilities)
+            selected_individual = population[selected_index]
 
-            # Generate a neighbor solution
-            new_solution = generate_neighbor_solution(selected_solution, population)
-            if objective_function(new_solution) < objective_function(selected_solution):
-                population[selected_index] = new_solution
+            # Generate a neighbor individual
+            new_individual = generate_neighbor_individual(selected_individual, population)
+            if objective_function(new_individual) < objective_function(selected_individual):
+                population[selected_index] = new_individual
 
         # Scout bees phase
-        best_solution = min(population, key=objective_function)
-        for i in range(n_employed_bees):
-            if objective_function(population[i]) > objective_function(best_solution):
+        best_individual = min(population, key=objective_function)
+        for i in range(n_employed_individuals):
+            if objective_function(population[i]) > objective_function(best_individual):
                 population[i] = np.random.uniform(lower_bound, upper_bound)
 
         # Print the best solution in the current iteration
-        print(f"Iteration {iteration + 1}: Best solution = {objective_function(best_solution)}")
+        print(f"Iteration {iteration + 1}: Best solution = {objective_function(best_individual)}")
 
     # Return the best solution found
-    return best_solution
+    return best_individual
 
-# Generate a neighbor solution for a given solution
-def generate_neighbor_solution(solution, population):
-    neighbor = solution
-    while neighbor is solution:
+# Generate a neighbor individual for a given individual
+def generate_neighbor_individual(individual, population):
+    neighbor = individual
+    while neighbor is individual:
         neighbor = np.random.choice(population)
     return neighbor
 
 # Example usage
-best_solution = artificial_bee_colony(n_iterations=10, n_employed_bees=30, n_onlooker_bees=30, n_trials=3)
-print(f"Best solution found: {objective_function(best_solution)}")
+best_individual = artificial_bee_colony(n_iterations=10, n_employed_individuals=30, n_onlooker_individuals=30, n_trials=3)
+print(f"Best solution found: {objective_function(best_individual)}")
