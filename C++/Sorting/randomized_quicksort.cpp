cf0f3cc80247b5ad51f02b3d496342411643fb8d
Dhruti Desai <106862356+dhruti26@users.noreply.github.com>
2023-07-08 22:34:21 +0530

    Update randomized_quicksort.cpp

--- C++/Sorting/randomized_quicksort.cpp
+++ C++/Sorting/randomized_quicksort.cpp
@@ -21,6 +21,7 @@ int Partition(int arr[],int p,int r){
 int Randomized_partition(int arr[],int p,int r){
 	//rand()%(r-p+1) generate random number in range 0 to r-p
 	// we want in range (p,r) so add p
+        srand(time(0)); //generate random seed
 	int i=p+rand()%(r-p+1);
 	swap(arr[i],arr[r]);
     
@@ -36,7 +37,6 @@ void Randomized_quicksort(int arr[],int p,int r){
 int main() {   
    int arr[]={78,9,10,-1,2,-45,91};
    int n = sizeof(arr)/sizeof(arr[0]);
-   srand(time(0)); //generate random seed
    Randomized_quicksort(arr,0,n-1) ; // arr,p,r;
    cout<<"Sorted array using Randomized quicksort is :"<<endl;
     for(int i=0;i<n;i++){
