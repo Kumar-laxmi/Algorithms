4dd715b06f8834560e6281c4b84b53de303a7d8d
Ryan <ryan.zy.cheng@gmail.com>
2023-07-08 02:01:01 -0700

    feat: completed lz78 decompression algorithm in c++

--- C++/Data Compression/LZ78.cc
+++ C++/Data Compression/LZ78.cc
@@ -43,17 +43,35 @@ vector <pair <int, char>> compress(string data) {
         }
         
     }
-    return compressed_data;
 
+    return compressed_data;
 }
+
+string decompress(vector <pair <int, char>> data) {
+    map <int, string> storage;
+    string decompressed_data = "";
+    int index = 1;
+    for (auto character: data) {
+        storage[index] = storage[character.first] + character.second;
+        decompressed_data += storage[index];
+        index += 1;
+    }
+
+    return decompressed_data;
+}
+
 
 int main() {
     string message = "ABBCBCABABCAABCAAB";
     vector <pair <int, char>> compressed_message = compress(message);
+    string decompressed_message = decompress(compressed_message);
 
+    cout << "Original Message: " << message << endl;
+    cout << "Compressed Message: ";
     for (auto element: compressed_message) {
-        cout << "(" << element.first << ", " << element.second << ") ";
+        cout << "(" << element.first << ", " << element.second << "), ";
     }
     cout << endl;
+    cout << "Decompresed Message: " << decompressed_message << endl;
 
 }
