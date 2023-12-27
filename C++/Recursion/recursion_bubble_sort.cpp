58bd5c4691e9078fcc1005a915304c69e9c45ab2
Shivam <shivamaher100@gmail.com>
2023-12-27 21:45:36 +0530

    Bubble-Sort-Using-recursion

---
+++ C++/Recursion/recursion_bubble_sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bubble(vector<int> &arr, int length)
+{
+    if(length == 1) return ;
+    int didswap = 0; 
+    for(int i = 0 ; i < length-1; i++)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            swap(arr[i],arr[i+1]);
+            didswap = 1;
+        }
+    }
+    if(didswap == 0) return;
+
+    bubble(arr,--length);
+
+}
+
+int main()
+{
+    vector<int> arr = {100, 23, 47, 99, 9, 25};
+    bubble(arr,arr.size());
+
+    
+    for (auto it : arr)
+    {
+        cout << it << endl;
+    }
+    return 0;
+}
